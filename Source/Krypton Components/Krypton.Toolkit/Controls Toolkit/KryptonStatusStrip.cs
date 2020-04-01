02ad7b9a1402ee8f7a6163e86f073d437420d719
Peter Wagner <peterwagner@live.co.uk>
2020-04-01 19:18:53 +0100

    Create KryptonStatusStrip.cs

---
+++ Source/Krypton Components/Krypton.Toolkit/Controls Toolkit/KryptonStatusStrip.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Krypton.Toolkit
+{
+    public class KryptonStatusStrip : StatusStrip
+    {
+        #region Variables
+        private ToolStripProgressBar[] _progressBars;
+        #endregion
+
+        #region Properties
+        public ToolStripProgressBar[] ProgressBars { get => _progressBars; set => _progressBars = value; }
+        #endregion
+
+        #region Constructor
+        public KryptonStatusStrip()
+        {
+
+        }
+        #endregion
+
+        #region Overrides
+        protected override void OnRendererChanged(EventArgs e)
+        {
+            if (ToolStripManager.Renderer is KryptonProfessionalRenderer kpr)
+            {
+                foreach (ToolStripProgressBar progressBar in ProgressBars)
+                {
+                    progressBar.BackColor = kpr.KCT.StatusStripGradientEnd;
+                }
+            }
+
+            base.OnRendererChanged(e);
+        }
+        #endregion
+    }
+}
