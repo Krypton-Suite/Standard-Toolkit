90191c648fd582fe325844ed38fcd1a02b88d01d
Peter Wagner <peterwagner@live.co.uk>
2021-03-28 09:08:07 +0100

    * License header updates

--- Source/Krypton Components/Krypton.Ribbon/ButtonSpec/ButtonSpecMdiChildClose.cs
+++ Source/Krypton Components/Krypton.Ribbon/ButtonSpec/ButtonSpecMdiChildClose.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿#region BSD License
+/*
+ * 
+ * Original BSD 3-Clause License (https://github.com/ComponentFactory/Krypton/blob/master/LICENSE)
+ *  © Component Factory Pty Ltd, 2006 - 2016, All rights reserved.
+ * 
+ *  New BSD 3-Clause License (https://github.com/Krypton-Suite/Standard-Toolkit/blob/master/LICENSE)
+ *  Modifications by Peter Wagner(aka Wagnerp) & Simon Coghlan(aka Smurf-IV), et al. 2017 - 2021. All rights reserved. 
+ *  Version 6.0.0  
+ *
+ */
+#endregion
+
+using System;
 using System.Diagnostics;
 using Krypton.Toolkit;
 
