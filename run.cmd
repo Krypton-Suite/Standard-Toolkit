a00cac2fd28d7b6a2653f214452ebb047394e82d
Peter Wagner <peterwagner@live.co.uk>
2021-06-10 17:38:20 +0100

    Update run.cmd

--- run.cmd
+++ run.cmd
@@ -1,5 +1,27 @@
 @echo off
 
+echo You are about to delete the Bin folder; do you want to continue? (Y/N)
+set INPUT=
+set /P INPUT=Type input: %=%
+if /I "%INPUT%"=="y" goto yes
+if /I "%INPUT%"=="n" goto no
+
+:yes
+rd /s /q "Bin"
+rd /s /q "Source\Krypton Components\Krypton.Docking\obj"
+rd /s /q "Source\Krypton Components\Krypton.Navigator\obj"
+rd /s /q "Source\Krypton Components\Krypton.Ribbon\obj"
+rd /s /q "Source\Krypton Components\Krypton.Toolkit\obj"
+rd /s /q "Source\Krypton Components\Krypton.Workspace\obj"
+
+:no
+echo Do you now want to build the repository? (y/n)
+set INPUT=
+set /PINPUT=Type input: %=%
+if /I "%INPUT%"=="y" goto buildproject
+if /I "%INPUT%"=="n" goto break
+
+:buildproject
 if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Preview\MSBuild\Current\Bin" goto vs16prev
 if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin" goto vs16ent
 if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin" goto vs16pro
@@ -11,45 +33,41 @@ pause
 
 :vs16prev
 set msbuildpath=%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Preview\MSBuild\Current\Bin
-goto purge
+goto build
 
 :vs16ent
 set msbuildpath=%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin
-goto purge
+goto build
 
 :vs16pro
 set msbuildpath=%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin
-goto purge
+goto build
 
 :vs16com
 set msbuildpath=%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin
-goto purge
+goto build
 
 :vs16build
 set msbuildpath=%ProgramFiles(x86)%\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin
-goto purge
+goto build
 
 :build
 set targets=Build
 if not "%~1" == "" set targets=%~1
 "%msbuildpath%\msbuild.exe" /t:%targets% build.proj /fl /flp:logfile=build.log
-goto pack
 
-:purge
-echo You are about to delete the Bin folder; do you want to continue? (Y/N)
+echo Plese alter file '{Path}\Directory.Build.props' before executing 'publish.cmd' script!
+
+echo Do you now want to create NuGet packages? (y/n)
 set INPUT=
-set /P INPUT=Type input: %=%
-if /I "%INPUT%"=="y" goto yes
-if /I "%INPUT%"=="n" goto no
+set /PINPUT=Type input: %=%
+if /I "%INPUT%"=="y" goto createpackages
+if /I "%INPUT%"=="n" goto break
 
-:yes
-rd /s /q "Bin"
-goto build
-
-:no
-goto build
-
-:pack
+:createpackages
 build.cmd Pack
 
+pause
+
+:break
 pause
