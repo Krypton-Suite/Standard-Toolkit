8eddbb338d73f050eec22822800822e2f605a79d
Peter Wagner <peterwagner@live.co.uk>
2021-07-25 17:25:26 +0100

    * It just works! ;)

--- Source/Krypton Components/Krypton.Toolkit/Converters/PaletteModeManagerConverter.cs
+++ Source/Krypton Components/Krypton.Toolkit/Converters/PaletteModeManagerConverter.cs
@@ -21,6 +21,7 @@ namespace Krypton.Toolkit
         private readonly Pair[] _pairs = new Pair[] { new(PaletteModeManager.ProfessionalSystem,    "Professional - System"),
                                              new(PaletteModeManager.ProfessionalOffice2003,"Professional - Office 2003"),
                                              new(PaletteModeManager.Office2007Blue,        "Office 2007 - Blue"),
+                                             new(PaletteModeManager.Office2007BlueAlt,     "Office 2007 - Blue (Alternative)"),
                                              new(PaletteModeManager.Office2007Silver,      "Office 2007 - Silver"),
                                              new(PaletteModeManager.Office2007White,       "Office 2007 - White"),
                                              new(PaletteModeManager.Office2007Black,       "Office 2007 - Black"),
