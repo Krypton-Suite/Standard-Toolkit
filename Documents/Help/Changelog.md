9ddfeac2a70357248b9875d294a0abc5ed09e3b0
Peter Wagner <peterwagner@live.co.uk>
2021-09-18 11:22:20 +0100

    * KryptonThemeComboBox

--- Documents/Help/Changelog.md
+++ Documents/Help/Changelog.md
@@ -1,6 +1,7 @@
 # <img src="https://github.com/Krypton-Suite/Standard-Toolkit/blob/master/Krypton.png"> Standard Toolkit - Changelog
 
 ## 2021-10-01 - Build 2110 - October 2021 (Canary)
+* New `KryptonThemeComboBox` control, switch between themes with a few clicks
 * New `KryptonColorButton` action list options
 * The `KryptonColorButton` now uses the `KryptonColorDialog`
 * Fixed [#361](https://github.com/Krypton-Suite/Standard-Toolkit/issues/361), Nightlies version needs to have something that allows installers to upgrade correctly
---
+++ Source/Krypton Components/Krypton.Toolkit/Controls Toolkit/KryptonThemeComboBox.cs
@@ -0,0 +1,29 @@
+﻿#region BSD License
+/*
+ * 
+ * Original BSD 3-Clause License (https://github.com/ComponentFactory/Krypton/blob/master/LICENSE)
+ *  © Component Factory Pty Ltd, 2006 - 2016, (Version 4.5.0.0) All rights reserved.
+ * 
+ *  New BSD 3-Clause License (https://github.com/Krypton-Suite/Standard-Toolkit/blob/master/LICENSE)
+ *  Modifications by Peter Wagner(aka Wagnerp) & Simon Coghlan(aka Smurf-IV), et al. 2017 - 2021. All rights reserved. 
+ *  
+ */
+#endregion
+
+namespace Krypton.Toolkit
+{
+    public class KryptonThemeComboBox : KryptonComboBox
+    {
+        #region Constructor
+        /// <summary>Initializes a new instance of the <see cref="KryptonThemeComboBox" /> class.</summary>
+        public KryptonThemeComboBox()
+        {
+            DropDownStyle = ComboBoxStyle.DropDownList;
+
+            ThemeManager.PropagateThemeSelector(this);
+
+            SelectedIndex = 24;
+        }
+        #endregion
+    }
+}
