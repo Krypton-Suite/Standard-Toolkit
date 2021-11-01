a5ba59f9777fc9bbee0e34ddd430a2bce1a79a5d
Peter Wagner <peterwagner@live.co.uk>
2021-11-01 08:26:25 +0000

    * Build 2111

---
+++ Source/Krypton Components/Krypton.Ribbon/Controls Ribbon Install/KryptonGalleryLicence.cs
@@ -0,0 +1,34 @@
+﻿// *****************************************************************************
+// 
+//  © Component Factory Pty Ltd, 2006 - 2016. All rights reserved.
+//	The software and associated documentation supplied hereunder are the 
+//  proprietary information of Component Factory Pty Ltd, PO Box 1504, 
+//  Glen Waverley, Vic 3150, Australia and are supplied subject to licence terms.
+// 
+//  Version 4.4.0.0 	
+// *****************************************************************************
+
+using System;
+using System.Drawing;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Reflection;
+using Krypton.Toolkit;
+
+namespace Krypton.Ribbon
+{
+	// TODO: What is this file for?
+    internal static class KryptonGalleryLicence
+    {
+        #region Static Methods
+        /// <summary>
+        /// Perform licence checking actions.
+        /// </summary>
+        /// <param name="gallery">Gallery control reference.</param>
+        internal static void PerformLicenceChecking(KryptonGallery gallery)
+        {
+        }
+        #endregion
+    }
+}
