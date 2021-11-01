6321759e00f0b71ab35f6e74d453695e336b658a
Peter Wagner <peterwagner@live.co.uk>
2021-11-01 08:22:24 +0000

    * Commit

---
+++ Source/Krypton Components/Krypton.Toolkit/General/XmlHelper.cs
@@ -0,0 +1,97 @@
+﻿namespace Krypton.Toolkit
+{
+    /// <summary>
+    /// Explicit helpers for XML Data export and import
+    /// </summary>
+    public static class XmlHelper
+    {
+        /// <summary>
+        /// Only persist the provided name/value pair as an Xml attribute if the value is not null/empty and not the default.
+        /// </summary>
+        /// <param name="xmlWriter">Xml writer to save information into.</param>
+        /// <param name="name">Attribute name.</param>
+        /// <param name="value">Attribute value.</param>
+        /// <param name="defaultValue">Default value.</param>
+        public static void TextToXmlAttribute(XmlWriter xmlWriter, string name, string value, string defaultValue = @"")
+        {
+            if (!string.IsNullOrEmpty(value) && (value != defaultValue))
+            {
+                xmlWriter.WriteAttributeString(name, value);
+            }
+        }
+
+        /// <summary>
+        /// Read the named attribute value but if no attribute is found then return the provided default.
+        /// </summary>
+        /// <param name="xmlReader">Xml reader to load information from.</param>
+        /// <param name="name">Attribute name.</param>
+        /// <param name="defaultValue">Default value.</param>
+        /// <returns></returns>
+        public static string XmlAttributeToText(XmlReader xmlReader, string name, string defaultValue = @"")
+        {
+            try
+            {
+                var ret = xmlReader.GetAttribute(name) ?? defaultValue;
+
+                return ret;
+            }
+            catch
+            {
+                return defaultValue;
+            }
+        }
+
+
+        /// <summary>
+        /// Convert a Image to a culture invariant string value.
+        /// </summary>
+        /// <param name="xmlWriter">Xml writer to save information into.</param>
+        /// <param name="name">Name of image to save.</param>
+        /// <param name="image">Image to persist.</param>
+        public static void ImageToXmlCData(XmlWriter xmlWriter, string name, Bitmap image)
+        {
+            // Only store if we have an actual image to persist
+            if (image != null)
+            {
+                // Convert the Image into base64 so it can be used in xml
+                MemoryStream memory = new();
+                image.Save(memory, image.RawFormat);
+                memory.Position = 0;
+                var base64 = Convert.ToBase64String(memory.ToArray());
+
+                // Store the base64 Hex as a CDATA inside the element
+                xmlWriter.WriteStartElement(name);
+                xmlWriter.WriteCData(base64);
+                xmlWriter.WriteEndElement();
+            }
+        }
+
+        /// <summary>
+        /// Convert a culture invariant string value into an Image.
+        /// </summary>
+        /// <param name="xmlReader">Xml reader to load information from.</param>
+        /// <returns>Image that was recreated.</returns>
+        public static Bitmap XmlCDataToImage(XmlReader xmlReader)
+        {
+            // Convert the content of the element into base64
+            var bytes = Convert.FromBase64String(xmlReader.ReadContentAsString());
+
+            // Convert the bytes back into an Image
+            using MemoryStream memory = new(bytes);
+            try
+            {
+                return new Bitmap(memory);
+            }
+            catch
+            {
+                // Do the old image way
+                // SYSLIB0011: BinaryFormatter serialization is obsolete
+#pragma warning disable SYSLIB0011
+                BinaryFormatter formatter = new();
+                var img = (Image)formatter.Deserialize(memory);
+#pragma warning restore SYSLIB0011
+                return new Bitmap(img);
+            }
+        }
+    }
+}
---
+++ publish-to-github.cmd
@@ -0,0 +1,3 @@
+@echo off
+
+dotnet nuget push "bin/Release/Standard-Toolkit.6.0.2006.2.nupkg" --source "github"
