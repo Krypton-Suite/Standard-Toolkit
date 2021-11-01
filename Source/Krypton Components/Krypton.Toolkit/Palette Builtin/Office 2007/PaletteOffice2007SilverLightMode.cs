8dfb9ad2f213ef4730ec04262de003806d58139a
Peter Wagner <peterwagner@live.co.uk>
2021-11-01 12:35:50 +0000

    * Fixes

--- Source/Krypton Components/Krypton.Toolkit/Palette Builtin/Office 2007/PaletteOffice2007SilverLightMode.cs
+++ Source/Krypton Components/Krypton.Toolkit/Palette Builtin/Office 2007/PaletteOffice2007SilverLightMode.cs
@@ -16,7 +16,7 @@ namespace Krypton.Toolkit
     /// <summary>
     /// Provides the Silver color scheme variant of the Office 2007 palette.
     /// </summary>
-    public class PaletteOffice2007SilverLightMode : PaletteOffice2007Base
+    public class PaletteOffice2007SilverLightMode : PaletteOffice2007SilverLightModeBase
     {
         #region Static Fields
         private static readonly ImageList _checkBoxList;
