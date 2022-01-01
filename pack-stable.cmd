e336d7e9fff52306b65f7f1b6da13406de3ff299
Wagnerp <peterwagner@live.co.uk>
2022-01-01 11:28:23 +0000

    * Scripts

--- pack-stable.cmd
+++ pack-stable.cmd
@@ -1,5 +1,7 @@
 @REM This command build the toolkit and create nuget packages
 
+@echo off
+
 echo Do you want to pack using Visual Studio 2019 or 2022? (2019/2022)
 set INPUT=
 set /P INPUT=Type 2019 or 2022: %=%
@@ -7,7 +9,7 @@ if /I "%INPUT%"=="2019" goto vs2019pack
 if /I "%INPUT%"=="2022" goto vs2022pack
 
 :vs2019pack
-build-2019.cmd Pack
+build-stable-2019.cmd Pack
 
 :vs2022pack
-build-2022.cmd Pack
+build-stable-2022.cmd Pack
