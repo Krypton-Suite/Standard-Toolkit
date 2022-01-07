1b17d95d53b8a4afa6a78e6acbb82cc63b653a10
Wagnerp <peterwagner@live.co.uk>
2022-01-07 17:14:25 +0000

    * OK07/10 Palettes

--- Source/Krypton Components/Krypton.Toolkit/Palette Builtin/Office 2007/PaletteOffice2007SilverDarkMode.cs
+++ Source/Krypton Components/Krypton.Toolkit/Palette Builtin/Office 2007/PaletteOffice2007SilverDarkMode.cs
@@ -3843,7 +3843,7 @@ namespace Krypton.Toolkit
         }
 
         /// <summary>
-        /// Gets the edge to position the button against.
+        /// Gets the edge to positon the button against.
         /// </summary>
         /// <param name="style">Style of button spec.</param>
         /// <returns>PaletteRelativeEdgeAlign value.</returns>
