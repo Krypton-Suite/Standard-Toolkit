0c5659f7e7d2bf6d923abb1e87d732bbeee02141
Peter Wagner <peterwagner@live.co.uk>
2022-10-06 07:39:33 +0100

    * Update license

--- Source/Krypton Components/Krypton.Navigator/View Draw/ViewDrawNavCheckButtonTab.cs
+++ Source/Krypton Components/Krypton.Navigator/View Draw/ViewDrawNavCheckButtonTab.cs
@@ -1,4 +1,16 @@
-﻿namespace Krypton.Navigator
+﻿#region BSD License
+/*
+ * 
+ * Original BSD 3-Clause License (https://github.com/ComponentFactory/Krypton/blob/master/LICENSE)
+ *  © Component Factory Pty Ltd, 2006 - 2016, (Version 4.5.0.0) All rights reserved.
+ * 
+ *  New BSD 3-Clause License (https://github.com/Krypton-Suite/Standard-Toolkit/blob/master/LICENSE)
+ *  Modifications by Peter Wagner(aka Wagnerp) & Simon Coghlan(aka Smurf-IV), et al. 2017 - 2023. All rights reserved. 
+ *  
+ */
+#endregion
+
+namespace Krypton.Navigator
 {
     /// <summary>
     /// Navigator view element for drawing a tab check button for a krypton page.
