412f4d560766367ed17df3d2c5efbd5f7acc4cbe
Peter Wagner <peterwagner@live.co.uk>
2022-10-06 07:47:52 +0100

    * Update license

--- Source/Krypton Components/Krypton.Ribbon/Palette/PaletteRibbonContextDouble.cs
+++ Source/Krypton Components/Krypton.Ribbon/Palette/PaletteRibbonContextDouble.cs
@@ -1,4 +1,18 @@
-﻿namespace Krypton.Ribbon
+﻿#region BSD License
+/*
+ * 
+ * Original BSD 3-Clause License (https://github.com/ComponentFactory/Krypton/blob/master/LICENSE)
+ *  © Component Factory Pty Ltd, 2006 - 2016, All rights reserved.
+ * 
+ *  New BSD 3-Clause License (https://github.com/Krypton-Suite/Standard-Toolkit/blob/master/LICENSE)
+ *  Modifications by Peter Wagner(aka Wagnerp) & Simon Coghlan(aka Smurf-IV), et al. 2017 - 2023. All rights reserved. 
+ *  
+ *  Modified: Monday 12th April, 2021 @ 18:00 GMT
+ *
+ */
+#endregion
+
+namespace Krypton.Ribbon
 {
     /// <summary>
     /// Return inherited values unless empty in which case return the context color.
