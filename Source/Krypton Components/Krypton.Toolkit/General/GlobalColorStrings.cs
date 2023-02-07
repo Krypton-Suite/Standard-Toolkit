f059704b45d6999ccbaa4e96db3a14a772219a2c
Peter Wagner <peterwagner@live.co.uk>
2023-02-07 18:23:18 +0000

    * `GlobalColorStrings`

---
+++ Source/Krypton Components/Krypton.Toolkit/General/GlobalColorStrings.cs
@@ -0,0 +1,23 @@
+﻿#region BSD License
+/*
+ * 
+ *  New BSD 3-Clause License (https://github.com/Krypton-Suite/Standard-Toolkit/blob/master/LICENSE)
+ *  Modifications by Peter Wagner(aka Wagnerp) & Simon Coghlan(aka Smurf-IV), et al. 2017 - 2023. All rights reserved. 
+ *  
+ */
+#endregion
+
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+// ReSharper disable MemberCanBeInternal
+
+namespace Krypton.Toolkit
+{
+    /// <summary>
+    /// Expose a global set of color strings used within Krypton and that are localizable.
+    /// </summary>
+    [TypeConverter(typeof(ExpandableObjectConverter))]
+    public class GlobalColorStrings : GlobalId
+    {
+    }
+}
