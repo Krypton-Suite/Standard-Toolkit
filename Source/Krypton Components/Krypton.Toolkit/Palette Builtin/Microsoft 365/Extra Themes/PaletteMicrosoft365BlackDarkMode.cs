9129bfdbfe710359cd3f7bde377f1a3361ea360c
Peter Wagner <peterwagner@live.co.uk>
2023-03-19 11:44:35 +0000

    * Office 2016 toolbar icons

--- Source/Krypton Components/Krypton.Toolkit/Palette Builtin/Microsoft 365/Extra Themes/PaletteMicrosoft365BlackDarkMode.cs
+++ Source/Krypton Components/Krypton.Toolkit/Palette Builtin/Microsoft 365/Extra Themes/PaletteMicrosoft365BlackDarkMode.cs
@@ -51,41 +51,61 @@ namespace Krypton.Toolkit
 
         #region Toolbar Images
 
-        private static readonly Image _formToolbarButtonSpecNewNormal = GenericToolbarImageResources.NewDocument;
-        private static readonly Image _formToolbarButtonSpecNewHover = GenericToolbarImageResources.NewDocument;
-        private static readonly Image _formToolbarButtonSpecNewDisabled = GenericToolbarImageResources.NewDocument;
+        private static readonly Image _formToolbarButtonSpecNewNormal = Microsoft365ToolbarImageResources.Office2016ToolbarNewNormal;
+        private static readonly Image _formToolbarButtonSpecNewActive = Microsoft365ToolbarImageResources.Office2016ToolbarNewNormal;
+        private static readonly Image _formToolbarButtonSpecNewDisabled = Microsoft365ToolbarImageResources.Office2016ToolbarNewDisabled;
 
-        private static readonly Image _formToolbarButtonSpecOpenNormal = GenericToolbarImageResources.Open;
-        private static readonly Image _formToolbarButtonSpecOpenHover = GenericToolbarImageResources.Open;
-        private static readonly Image _formToolbarButtonSpecOpenDisabled = GenericToolbarImageResources.Open;
+        private static readonly Image _formToolbarButtonSpecOpenNormal = Microsoft365ToolbarImageResources.Office2016ToolbarOpenNormal;
+        private static readonly Image _formToolbarButtonSpecOpenActive = Microsoft365ToolbarImageResources.Office2016ToolbarOpenNormal;
+        private static readonly Image _formToolbarButtonSpecOpenDisabled = Microsoft365ToolbarImageResources.Office2016ToolbarOpenDisabled;
 
-        private static readonly Image _formToolbarButtonSpecSaveNormal = GenericToolbarImageResources.Save;
-        private static readonly Image _formToolbarButtonSpecSaveHover = GenericToolbarImageResources.Save;
-        private static readonly Image _formToolbarButtonSpecSaveDisabled = GenericToolbarImageResources.Save;
+        private static readonly Image _formToolbarButtonSpecSaveNormal = Microsoft365ToolbarImageResources.Office2016ToolbarSaveNormal;
+        private static readonly Image _formToolbarButtonSpecSaveActive = Microsoft365ToolbarImageResources.Office2016ToolbarSaveNormal;
+        private static readonly Image _formToolbarButtonSpecSaveDisabled = Microsoft365ToolbarImageResources.Office2016ToolbarSaveDisabled;
 
-        private static readonly Image _formToolbarButtonSpecSaveAsNormal = GenericToolbarImageResources.SaveAs;
-        private static readonly Image _formToolbarButtonSpecSaveAsHover = GenericToolbarImageResources.SaveAs;
-        private static readonly Image _formToolbarButtonSpecSaveAsDisabled = GenericToolbarImageResources.SaveAs;
+        private static readonly Image _formToolbarButtonSpecSaveAllNormal = Microsoft365ToolbarImageResources.Office2016ToolbarSaveAllNormal;
+        private static readonly Image _formToolbarButtonSpecSaveAllActive = Microsoft365ToolbarImageResources.Office2016ToolbarSaveAllNormal;
+        private static readonly Image _formToolbarButtonSpecSaveAllDisabled = Microsoft365ToolbarImageResources.Office2016ToolbarSaveAllDisabled;
 
-        private static readonly Image _formToolbarButtonSpecCutNormal = GenericToolbarImageResources.Cut;
-        private static readonly Image _formToolbarButtonSpecCutHover = GenericToolbarImageResources.Cut;
-        private static readonly Image _formToolbarButtonSpecCutDisabled = GenericToolbarImageResources.Cut;
+        private static readonly Image _formToolbarButtonSpecSaveAsNormal = Microsoft365ToolbarImageResources.Office2016ToolbarSaveAsNormal;
+        private static readonly Image _formToolbarButtonSpecSaveAsActive = Microsoft365ToolbarImageResources.Office2016ToolbarSaveAsNormal;
+        private static readonly Image _formToolbarButtonSpecSaveAsDisabled = Microsoft365ToolbarImageResources.Office2016ToolbarSaveAsDisabled;
 
-        private static readonly Image _formToolbarButtonSpecCopyNormal = GenericToolbarImageResources.Copy;
-        private static readonly Image _formToolbarButtonSpecCopyHover = GenericToolbarImageResources.Copy;
-        private static readonly Image _formToolbarButtonSpecCopyDisabled = GenericToolbarImageResources.Copy;
+        private static readonly Image _formToolbarButtonSpecCutNormal = Microsoft365ToolbarImageResources.Office2016ToolbarCutNormal;
+        private static readonly Image _formToolbarButtonSpecCutActive = Microsoft365ToolbarImageResources.Office2016ToolbarCutNormal;
+        private static readonly Image _formToolbarButtonSpecCutDisabled = Microsoft365ToolbarImageResources.Office2016ToolbarCutDisabled;
 
-        private static readonly Image _formToolbarButtonSpecPasteNormal = GenericToolbarImageResources.Paste;
-        private static readonly Image _formToolbarButtonSpecPasteHover = GenericToolbarImageResources.Paste;
-        private static readonly Image _formToolbarButtonSpecPasteDisabled = GenericToolbarImageResources.Paste;
+        private static readonly Image _formToolbarButtonSpecCopyNormal = Microsoft365ToolbarImageResources.Office2016ToolbarCopyNormal;
+        private static readonly Image _formToolbarButtonSpecCopyActive = Microsoft365ToolbarImageResources.Office2016ToolbarCopyNormal;
+        private static readonly Image _formToolbarButtonSpecCopyDisabled = Microsoft365ToolbarImageResources.Office2016ToolbarCopyDisabled;
 
-        private static readonly Image _formToolbarButtonSpecUndoNormal = GenericToolbarImageResources.Undo;
-        private static readonly Image _formToolbarButtonSpecUndoHover = GenericToolbarImageResources.Undo;
-        private static readonly Image _formToolbarButtonSpecUndoDisabled = GenericToolbarImageResources.Undo;
+        private static readonly Image _formToolbarButtonSpecPasteNormal = Microsoft365ToolbarImageResources.Office2016ToolbarPasteNormal;
+        private static readonly Image _formToolbarButtonSpecPasteActive = Microsoft365ToolbarImageResources.Office2016ToolbarPasteNormal;
+        private static readonly Image _formToolbarButtonSpecPasteDisabled = Microsoft365ToolbarImageResources.Office2016ToolbarPasteDisabled;
 
-        private static readonly Image _formToolbarButtonSpecRedoNormal = GenericToolbarImageResources.Redo;
-        private static readonly Image _formToolbarButtonSpecRedoHover = GenericToolbarImageResources.Redo;
-        private static readonly Image _formToolbarButtonSpecRedoDisabled = GenericToolbarImageResources.Redo;
+        private static readonly Image _formToolbarButtonSpecUndoNormal = Microsoft365ToolbarImageResources.Office2016ToolbarUndoNormal;
+        private static readonly Image _formToolbarButtonSpecUndoActive = Microsoft365ToolbarImageResources.Office2016ToolbarUndoNormal;
+        private static readonly Image _formToolbarButtonSpecUndoDisabled = Microsoft365ToolbarImageResources.Office2016ToolbarUndoDisabled;
+
+        private static readonly Image _formToolbarButtonSpecRedoNormal = Microsoft365ToolbarImageResources.Office2016ToolbarRedoNormal;
+        private static readonly Image _formToolbarButtonSpecRedoActive = Microsoft365ToolbarImageResources.Office2016ToolbarRedoNormal;
+        private static readonly Image _formToolbarButtonSpecRedoDisabled = Microsoft365ToolbarImageResources.Office2016ToolbarRedoDisabled;
+
+        private static readonly Image _formToolbarButtonSpecPageSetupNormal = Microsoft365ToolbarImageResources.Office2016ToolbarPageSetupNormal;
+        private static readonly Image _formToolbarButtonSpecPageSetupActive = Microsoft365ToolbarImageResources.Office2016ToolbarPageSetupNormal;
+        private static readonly Image _formToolbarButtonSpecPageSetupDisabled = Microsoft365ToolbarImageResources.Office2016ToolbarPageSetupDisabled;
+
+        private static readonly Image _formToolbarButtonSpecPrintPreviewNormal = Microsoft365ToolbarImageResources.Office2016ToolbarPrintPreviewNormal;
+        private static readonly Image _formToolbarButtonSpecPrintPreviewActive = Microsoft365ToolbarImageResources.Office2016ToolbarPrintPreviewNormal;
+        private static readonly Image _formToolbarButtonSpecPrintPreviewDisabled = Microsoft365ToolbarImageResources.Office2016ToolbarPrintPreviewDisabled;
+
+        private static readonly Image _formToolbarButtonSpecPrintNormal = Microsoft365ToolbarImageResources.Office2016ToolbarPrintNormal;
+        private static readonly Image _formToolbarButtonSpecPrintActive = Microsoft365ToolbarImageResources.Office2016ToolbarPrintNormal;
+        private static readonly Image _formToolbarButtonSpecPrintDisabled = Microsoft365ToolbarImageResources.Office2016ToolbarPrintDisabled;
+
+        private static readonly Image _formToolbarButtonSpecQuickPrintNormal = Microsoft365ToolbarImageResources.Office2016ToolbarQuickPrintNormal;
+        private static readonly Image _formToolbarButtonSpecQuickPrintActive = Microsoft365ToolbarImageResources.Office2016ToolbarQuickPrintNormal;
+        private static readonly Image _formToolbarButtonSpecQuickPrintDisabled = Microsoft365ToolbarImageResources.Office2016ToolbarQuickPrintDisabled;
 
         #endregion
 
@@ -439,57 +459,87 @@ namespace Krypton.Toolkit
                 },
                 PaletteButtonSpecStyle.New => state switch
                 {
-                    PaletteState.Tracking => _formToolbarButtonSpecNewHover,
+                    PaletteState.Tracking => _formToolbarButtonSpecNewActive,
                     PaletteState.Normal => _formToolbarButtonSpecNewNormal,
                     _ => _formToolbarButtonSpecNewDisabled
                 },
                 PaletteButtonSpecStyle.Open => state switch
                 {
-                    PaletteState.Tracking => _formToolbarButtonSpecOpenHover,
+                    PaletteState.Tracking => _formToolbarButtonSpecOpenActive,
                     PaletteState.Normal => _formToolbarButtonSpecOpenNormal,
                     _ => _formToolbarButtonSpecOpenDisabled
                 },
                 PaletteButtonSpecStyle.Save => state switch
                 {
-                    PaletteState.Tracking => _formToolbarButtonSpecSaveHover,
+                    PaletteState.Tracking => _formToolbarButtonSpecSaveActive,
                     PaletteState.Normal => _formToolbarButtonSpecSaveNormal,
                     _ => _formToolbarButtonSpecSaveDisabled
                 },
                 PaletteButtonSpecStyle.SaveAs => state switch
                 {
-                    PaletteState.Tracking => _formToolbarButtonSpecSaveAsHover,
+                    PaletteState.Tracking => _formToolbarButtonSpecSaveAsActive,
                     PaletteState.Normal => _formToolbarButtonSpecSaveAsNormal,
                     _ => _formToolbarButtonSpecSaveAsDisabled
                 },
+                PaletteButtonSpecStyle.SaveAll => state switch
+                {
+                    PaletteState.Tracking => _formToolbarButtonSpecSaveAllActive,
+                    PaletteState.Normal => _formToolbarButtonSpecSaveAllNormal,
+                    _ => _formToolbarButtonSpecSaveAllDisabled
+                },
                 PaletteButtonSpecStyle.Cut => state switch
                 {
-                    PaletteState.Tracking => _formToolbarButtonSpecCutHover,
+                    PaletteState.Tracking => _formToolbarButtonSpecCutActive,
                     PaletteState.Normal => _formToolbarButtonSpecCutNormal,
                     _ => _formToolbarButtonSpecCutDisabled
                 },
                 PaletteButtonSpecStyle.Copy => state switch
                 {
-                    PaletteState.Tracking => _formToolbarButtonSpecCopyHover,
+                    PaletteState.Tracking => _formToolbarButtonSpecCopyActive,
                     PaletteState.Normal => _formToolbarButtonSpecCopyNormal,
                     _ => _formToolbarButtonSpecCopyDisabled
                 },
                 PaletteButtonSpecStyle.Paste => state switch
                 {
-                    PaletteState.Tracking => _formToolbarButtonSpecPasteHover,
+                    PaletteState.Tracking => _formToolbarButtonSpecPasteActive,
                     PaletteState.Normal => _formToolbarButtonSpecPasteNormal,
                     _ => _formToolbarButtonSpecPasteDisabled
                 },
                 PaletteButtonSpecStyle.Undo => state switch
                 {
-                    PaletteState.Tracking => _formToolbarButtonSpecUndoHover,
+                    PaletteState.Tracking => _formToolbarButtonSpecUndoActive,
                     PaletteState.Normal => _formToolbarButtonSpecUndoNormal,
                     _ => _formToolbarButtonSpecUndoDisabled
                 },
                 PaletteButtonSpecStyle.Redo => state switch
                 {
-                    PaletteState.Tracking => _formToolbarButtonSpecRedoHover,
+                    PaletteState.Tracking => _formToolbarButtonSpecRedoActive,
                     PaletteState.Normal => _formToolbarButtonSpecRedoNormal,
                     _ => _formToolbarButtonSpecRedoDisabled
+                },
+                PaletteButtonSpecStyle.PageSetup => state switch
+                {
+                    PaletteState.Tracking => _formToolbarButtonSpecPageSetupActive,
+                    PaletteState.Normal => _formToolbarButtonSpecPageSetupNormal,
+                    _ => _formToolbarButtonSpecPageSetupDisabled
+                },
+                PaletteButtonSpecStyle.PrintPreview => state switch
+                {
+                    PaletteState.Tracking => _formToolbarButtonSpecPrintPreviewActive,
+                    PaletteState.Normal => _formToolbarButtonSpecPrintPreviewNormal,
+                    _ => _formToolbarButtonSpecPrintPreviewDisabled
+                },
+                PaletteButtonSpecStyle.Print => state switch
+                {
+                    PaletteState.Tracking => _formToolbarButtonSpecPrintActive,
+                    PaletteState.Normal => _formToolbarButtonSpecPrintNormal,
+                    _ => _formToolbarButtonSpecPrintDisabled
+                },
+                PaletteButtonSpecStyle.QuickPrint => state switch
+                {
+                    PaletteState.Tracking => _formToolbarButtonSpecQuickPrintActive,
+                    PaletteState.Normal => _formToolbarButtonSpecQuickPrintNormal,
+                    _ => _formToolbarButtonSpecQuickPrintDisabled
                 },
                 PaletteButtonSpecStyle.RibbonMinimize => _buttonSpecRibbonMinimize,
                 PaletteButtonSpecStyle.RibbonExpand => _buttonSpecRibbonExpand,
--- Source/Krypton Components/Krypton.Toolkit/Resources/Microsoft365ToolbarImageResources.Designer.cs
+++ Source/Krypton Components/Krypton.Toolkit/Resources/Microsoft365ToolbarImageResources.Designer.cs
@@ -59,5 +59,285 @@ namespace Krypton.Toolkit.Resources {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Office2016ToolbarCopyDisabled {
+            get {
+                object obj = ResourceManager.GetObject("Office2016ToolbarCopyDisabled", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Office2016ToolbarCopyNormal {
+            get {
+                object obj = ResourceManager.GetObject("Office2016ToolbarCopyNormal", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Office2016ToolbarCutDisabled {
+            get {
+                object obj = ResourceManager.GetObject("Office2016ToolbarCutDisabled", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Office2016ToolbarCutNormal {
+            get {
+                object obj = ResourceManager.GetObject("Office2016ToolbarCutNormal", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Office2016ToolbarNewDisabled {
+            get {
+                object obj = ResourceManager.GetObject("Office2016ToolbarNewDisabled", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Office2016ToolbarNewNormal {
+            get {
+                object obj = ResourceManager.GetObject("Office2016ToolbarNewNormal", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Office2016ToolbarOpenDisabled {
+            get {
+                object obj = ResourceManager.GetObject("Office2016ToolbarOpenDisabled", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Office2016ToolbarOpenNormal {
+            get {
+                object obj = ResourceManager.GetObject("Office2016ToolbarOpenNormal", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Office2016ToolbarPageSetupDisabled {
+            get {
+                object obj = ResourceManager.GetObject("Office2016ToolbarPageSetupDisabled", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Office2016ToolbarPageSetupNormal {
+            get {
+                object obj = ResourceManager.GetObject("Office2016ToolbarPageSetupNormal", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Office2016ToolbarPasteDisabled {
+            get {
+                object obj = ResourceManager.GetObject("Office2016ToolbarPasteDisabled", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Office2016ToolbarPasteNormal {
+            get {
+                object obj = ResourceManager.GetObject("Office2016ToolbarPasteNormal", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Office2016ToolbarPrintDisabled {
+            get {
+                object obj = ResourceManager.GetObject("Office2016ToolbarPrintDisabled", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Office2016ToolbarPrintNormal {
+            get {
+                object obj = ResourceManager.GetObject("Office2016ToolbarPrintNormal", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Office2016ToolbarPrintPreviewDisabled {
+            get {
+                object obj = ResourceManager.GetObject("Office2016ToolbarPrintPreviewDisabled", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Office2016ToolbarPrintPreviewNormal {
+            get {
+                object obj = ResourceManager.GetObject("Office2016ToolbarPrintPreviewNormal", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Office2016ToolbarQuickPrintDisabled {
+            get {
+                object obj = ResourceManager.GetObject("Office2016ToolbarQuickPrintDisabled", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Office2016ToolbarQuickPrintNormal {
+            get {
+                object obj = ResourceManager.GetObject("Office2016ToolbarQuickPrintNormal", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Office2016ToolbarRedoDisabled {
+            get {
+                object obj = ResourceManager.GetObject("Office2016ToolbarRedoDisabled", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Office2016ToolbarRedoNormal {
+            get {
+                object obj = ResourceManager.GetObject("Office2016ToolbarRedoNormal", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Office2016ToolbarSaveAllDisabled {
+            get {
+                object obj = ResourceManager.GetObject("Office2016ToolbarSaveAllDisabled", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Office2016ToolbarSaveAllNormal {
+            get {
+                object obj = ResourceManager.GetObject("Office2016ToolbarSaveAllNormal", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Office2016ToolbarSaveAsDisabled {
+            get {
+                object obj = ResourceManager.GetObject("Office2016ToolbarSaveAsDisabled", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Office2016ToolbarSaveAsNormal {
+            get {
+                object obj = ResourceManager.GetObject("Office2016ToolbarSaveAsNormal", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Office2016ToolbarSaveDisabled {
+            get {
+                object obj = ResourceManager.GetObject("Office2016ToolbarSaveDisabled", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Office2016ToolbarSaveNormal {
+            get {
+                object obj = ResourceManager.GetObject("Office2016ToolbarSaveNormal", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Office2016ToolbarUndoDisabled {
+            get {
+                object obj = ResourceManager.GetObject("Office2016ToolbarUndoDisabled", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Office2016ToolbarUndoNormal {
+            get {
+                object obj = ResourceManager.GetObject("Office2016ToolbarUndoNormal", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
