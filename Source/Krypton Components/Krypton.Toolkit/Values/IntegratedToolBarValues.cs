d5da2661de3576fc015c7c7ea5505ff12aa12f82
Peter Wagner <peterwagner@live.co.uk>
2023-03-19 19:12:56 +0000

    * Tests

---
+++ Source/Krypton Components/Krypton.Toolkit/Values/IntegratedToolBarValues.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Krypton.Toolkit
+{
+    public class IntegratedToolBarValues : Storage
+    {
+        #region Instance Fields
+
+        private bool _showIntegratedToolbar;
+
+        private ButtonSpecAny[]? _integratedToolbarButtonCollection;
+
+        private KryptonForm _owner;
+
+        #endregion
+
+        public override bool IsDefault { get; }
+    }
+}
--- Source/Krypton Components/TestForm/Form4.Designer.cs
+++ Source/Krypton Components/TestForm/Form4.Designer.cs
@@ -36,6 +36,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Name = "Form4";
+            this.ShowIntegratedToolBar = true;
             this.Text = "Form4";
             this.ResumeLayout(false);
 
