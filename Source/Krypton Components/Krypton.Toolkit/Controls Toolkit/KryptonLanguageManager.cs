9065ed049760721495d665bb59ee5c966c8878e9
Peter Wagner <peterwagner@live.co.uk>
2023-04-23 11:11:24 +0100

    * More tidy up

--- Source/Krypton Components/Krypton.Toolkit/Controls Toolkit/KryptonLanguageManager.cs
+++ Source/Krypton Components/Krypton.Toolkit/Controls Toolkit/KryptonLanguageManager.cs
@@ -9,6 +9,7 @@
 
 namespace Krypton.Toolkit
 {
+    /// <summary>Access 'Global' Krypton string settings.</summary>
     [Category(@"Code")]
     [Description(@"Access 'Global' Krypton string settings.")]
     [ToolboxBitmap(typeof(KryptonLanguageManager), "ToolboxBitmaps.KryptonLanguageManager.bmp")]
@@ -22,6 +23,8 @@ namespace Krypton.Toolkit
 
         #region Public
 
+        /// <summary>Gets the global color strings.</summary>
+        /// <value>The global color strings.</value>
         [Category(@"Visuals")]
         [Description(@"Collection of color strings.")]
         [MergableProperty(false)]
@@ -31,8 +34,11 @@ namespace Krypton.Toolkit
 
         private bool ShouldSerializeColorStrings() => !ColorStrings.IsDefault;
 
+        /// <summary>Resets the color strings.</summary>
         public void ResetColorStrings() => ColorStrings.Reset();
 
+        /// <summary>Gets the button spec style strings.</summary>
+        /// <value>The button spec style strings.</value>
         [Category(@"Visuals")]
         [Description(@"Collection of button spec style strings.")]
         [MergableProperty(false)]
@@ -42,6 +48,7 @@ namespace Krypton.Toolkit
 
         private bool ShouldSerializeButtonSpecStyleStrings() => !SpecStyleStrings.IsDefault;
 
+        /// <summary>Resets the button spec style strings.</summary>
         public void ResetButtonSpecStyleStrings() => SpecStyleStrings.Reset();
 
         /// <summary>Gets the general strings.</summary>
@@ -55,43 +62,78 @@ namespace Krypton.Toolkit
 
         private bool ShouldSerializeGeneralStrings() => !Strings.IsDefault;
 
+        /// <summary>Resets the general strings.</summary>
         public void ResetGeneralStrings() => Strings.Reset();
 
+        /// <summary>Gets the palette mode strings.</summary>
+        /// <value>The palette mode strings.</value>
+        [Category(@"Visuals")]
+        [Description(@"Collection of palette mode strings.")]
+        [MergableProperty(false)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
+        [Localizable(true)]
         public PaletteModeStrings PaletteModeStrings => ModeStrings;
 
         private bool ShouldSerializePaletteModeStrings() => !ModeStrings.IsDefault;
 
+        /// <summary>Resets the palette mode strings.</summary>
         public void ResetPaletteModeStrings() => ModeStrings.Reset();
 
+        /// <summary>Gets the data grid view style strings.</summary>
+        /// <value>The data grid view style strings.</value>
+        [Category(@"Visuals")]
+        [Description(@"Collection of datagrid view style strings.")]
+        [MergableProperty(false)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
+        [Localizable(true)]
         public DataGridViewStyleStrings DataGridViewStyleStrings => GridViewStyleStrings;
 
         private bool ShouldSerializeDataGridViewStyleStrings() => !GridViewStyleStrings.IsDefault;
 
+        /// <summary>Resets the data grid view style strings.</summary>
         public void ResetDataGridViewStyleStrings() => GridViewStyleStrings.Reset();
 
+        /// <summary>Gets the grid style strings.</summary>
+        /// <value>The grid style strings.</value>
+        [Category(@"Visuals")]
+        [Description(@"Collection of grid view style strings.")]
+        [MergableProperty(false)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
+        [Localizable(true)]
         public GridStyleStrings GridStyleStrings => StyleStrings;
 
         private bool ShouldSerializeGridStyleStrings() => !StyleStrings.IsDefault;
 
+        /// <summary>Resets the grid style strings.</summary>
         public void ResetGridStyleStrings() => StyleStrings.Reset();
 
         #endregion
 
         #region Static Strings
 
+        /// <summary>Gets the color strings.</summary>
+        /// <value>The color strings.</value>
         public static GlobalColorStrings ColorStrings
         { get; } = new();
 
+        /// <summary>Gets the spec style strings.</summary>
+        /// <value>The spec style strings.</value>
         public static ButtonSpecStyleStrings SpecStyleStrings { get; } = new();
 
         /// <summary>Gets the strings.</summary>
         /// <value>The strings.</value>
         public static GeneralStrings Strings { get; } = new();
 
+        /// <summary>Gets the mode strings.</summary>
+        /// <value>The mode strings.</value>
         public static PaletteModeStrings ModeStrings { get; } = new();
 
+        /// <summary>Gets the grid view style strings.</summary>
+        /// <value>The grid view style strings.</value>
         public static DataGridViewStyleStrings GridViewStyleStrings { get; } = new();
 
+        /// <summary>Gets the style strings.</summary>
+        /// <value>The style strings.</value>
         public static GridStyleStrings StyleStrings { get; } = new();
 
         #endregion
@@ -108,6 +150,9 @@ namespace Krypton.Toolkit
 
         #region Implementation
 
+        /// <summary>Gets a value indicating whether this instance is default.</summary>
+        /// <value>
+        ///   <c>true</c> if this instance is default; otherwise, <c>false</c>.</value>
         [EditorBrowsable(EditorBrowsableState.Never)]
         [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
         public bool IsDefault => !(ShouldSerializeGeneralStrings() ||
