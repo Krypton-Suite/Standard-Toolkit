e895897a4f0726ea300269953e4ddfbd41bb0046
Peter Wagner <peterwagner@live.co.uk>
2023-06-13 11:37:00 +0100

    * `nullables`

--- Source/Krypton Components/Krypton.Navigator/Controls Navigator/KryptonNavigator.cs
+++ Source/Krypton Components/Krypton.Navigator/Controls Navigator/KryptonNavigator.cs
@@ -1933,7 +1933,7 @@ namespace Krypton.Navigator
             Debug.Assert(page != null);
 
             // Get the index of the page
-            var pos = Pages.IndexOf(page);
+            var pos = Pages.IndexOf(page!);
 
             // Search backwards towards start of pages collection
             for (var i = pos - 1; i >= 0; i--)
@@ -1953,7 +1953,7 @@ namespace Krypton.Navigator
             Debug.Assert(page != null);
 
             // Get the index of the page
-            var pos = Pages.IndexOf(page);
+            var pos = Pages.IndexOf(page!);
 
             // Search towards end of pages collection
             for (var i = pos + 1; i < Pages.Count; i++)
