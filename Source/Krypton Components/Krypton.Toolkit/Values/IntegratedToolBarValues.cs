f5690de3e00fcf1ff7647efca789d1e12b5f5c17
Peter Wagner <peterwagner@live.co.uk>
2023-06-18 18:45:12 +0100

    Create IntegratedToolBarValues.cs

---
+++ Source/Krypton Components/Krypton.Toolkit/Values/IntegratedToolBarValues.cs
@@ -0,0 +1,48 @@
+﻿#region BSD License
+/*
+ *  
+ *  New BSD 3-Clause License (https://github.com/Krypton-Suite/Standard-Toolkit/blob/master/LICENSE)
+ *  Modifications by Peter Wagner(aka Wagnerp) & Simon Coghlan(aka Smurf-IV), et al. 2017 - 2023. All rights reserved. 
+ *  
+ */
+#endregion
+
+namespace Krypton.Toolkit
+{
+    [Category(@"code")]
+    [ToolboxItem(false)]
+    public class IntegratedToolBarValues : Storage
+    {
+        #region Instance Fields
+
+
+
+        #endregion
+
+        #region Public
+
+
+
+        #endregion
+
+        #region Identity
+
+        public IntegratedToolBarValues()
+        {
+            Reset();
+        }
+
+        #endregion
+
+        #region Implementation
+
+        public override bool IsDefault { get; }
+
+        public void Reset()
+        {
+
+        }
+
+        #endregion
+    }
+}
