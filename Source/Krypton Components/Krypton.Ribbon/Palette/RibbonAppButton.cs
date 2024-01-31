53ba26c3387b0471127f594ad4b39e98965eaf40
Peter Wagner <peterwagner@live.co.uk>
2024-01-31 16:06:35 +0000

    Update RibbonAppButton.cs

--- Source/Krypton Components/Krypton.Ribbon/Palette/RibbonAppButton.cs
+++ Source/Krypton Components/Krypton.Ribbon/Palette/RibbonAppButton.cs
@@ -436,7 +436,11 @@ namespace Krypton.Ribbon
 
                     if (_ribbon.CaptionArea != null)
                     {
-                        _ribbon.CaptionArea.KryptonForm!.CloseBox = value;
+                        if (_ribbon.CaptionArea.KryptonForm != null)
+                        {
+                            _ribbon.CaptionArea.KryptonForm.CloseBox = value;
+                        }
+
                         _ribbon.CaptionArea.PerformFormChromeCheck();
                     }
                 }
