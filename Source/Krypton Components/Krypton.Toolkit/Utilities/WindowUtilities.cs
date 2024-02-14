ae60177c6285f590d2f4ecb279483a79d6cab4ef
Peter Wagner <peterwagner@live.co.uk>
2024-02-14 19:10:46 +0000

    Update WindowUtilities.cs

--- Source/Krypton Components/Krypton.Toolkit/Utilities/WindowUtilities.cs
+++ Source/Krypton Components/Krypton.Toolkit/Utilities/WindowUtilities.cs
@@ -1,8 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿#region BSD License
+/*
+ *
+ *  New BSD 3-Clause License (https://github.com/Krypton-Suite/Standard-Toolkit/blob/master/LICENSE)
+ *  Modifications by Peter Wagner(aka Wagnerp) & Simon Coghlan(aka Smurf-IV), et al. 2024 - 2024. All rights reserved.
+ *
+ */
+#endregion
 
 namespace Krypton.Toolkit
 {
