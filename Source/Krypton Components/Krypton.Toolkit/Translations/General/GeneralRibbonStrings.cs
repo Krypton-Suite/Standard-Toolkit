9e5f1791be867cfdcb80baf85245e41aa4aa3140
Peter Wagner <peterwagner@live.co.uk>
2024-02-29 09:02:18 +0000

    * Tabs

--- Source/Krypton Components/Krypton.Toolkit/Translations/General/GeneralRibbonStrings.cs
+++ Source/Krypton Components/Krypton.Toolkit/Translations/General/GeneralRibbonStrings.cs
@@ -15,6 +15,19 @@ namespace Krypton.Toolkit
         #region Static Values
 
         private const string DEFAULT_APPLICATION_BUTTON_TEXT = @"File";
+        private const string DEFAULT_APPLICATION_BUTTON_KEY_TIP = @"F";
+        private const string DEFAULT_CUSTOMIZE_QUICK_ACCESS_TOOLBAR = @"Customize Quick Access Toolbar";
+        private const string DEFAULT_MINIMIZE = @"Mi&nimize the Ribbon";
+        private const string DEFAULT_MORE_COLORS = @"&More Colors...";
+        private const string DEFAULT_NO_COLOR = @"&No Color";
+        private const string DEFAULT_RECENT_DOCUMENTS = @"Recent Documents";
+        private const string DEFAULT_RECENT_COLORS = @"Recent Colors";
+        private const string DEFAULT_SHOW_QAT_ABOVE_RIBBON = @"&Show Quick Access Toolbar Above the Ribbon";
+        private const string DEFAULT_SHOW_QAT_BELOW_RIBBON = @"&Show Quick Access Toolbar Below the Ribbon";
+        private const string DEFAULT_SHOW_ABOVE_RIBBON = @"&Show Above the Ribbon";
+        private const string DEFAULT_SHOW_BELOW_RIBBON = @"&Show Below the Ribbon";
+        private const string DEFAULT_STANDARD_COLORS = @"Standard Colors";
+        private const string DEFAULT_THEME_COLORS = @"Theme Colors";
 
         #endregion
 
