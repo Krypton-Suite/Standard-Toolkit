8eaec7fd4058beb05d2bcaf3dd366c739bad6b7c
Peter Wagner <peterwagner@live.co.uk>
2024-02-29 08:58:13 +0000

    Create GeneralRibbonStrings.cs

---
+++ Source/Krypton Components/Krypton.Toolkit/Translations/General/GeneralRibbonStrings.cs
@@ -0,0 +1,53 @@
+﻿#region BSD License
+/*
+ *
+ *  New BSD 3-Clause License (https://github.com/Krypton-Suite/Standard-Toolkit/blob/master/LICENSE)
+ *  Modifications by Peter Wagner(aka Wagnerp) & Simon Coghlan(aka Smurf-IV), et al. 2024 - 2024. All rights reserved.
+ *
+ */
+#endregion
+
+namespace Krypton.Toolkit
+{
+    [TypeConverter(typeof(ExpandableObjectConverter))]
+    public class GeneralRibbonStrings : GlobalId
+    {
+        #region Static Values
+
+        private const string DEFAULT_APPLICATION_BUTTON_TEXT = @"File";
+
+        #endregion
+
+        #region Identity
+
+        /// <summary>Initializes a new instance of the <see cref="GeneralRibbonStrings" /> class.</summary>
+        public GeneralRibbonStrings()
+        {
+            Reset();
+        }
+
+        #endregion
+
+        #region IsDefault
+
+        [Browsable(false)] 
+        public bool IsDefault => false;
+
+        #endregion
+
+        #region Public
+
+
+
+        #endregion
+
+        #region Implementation
+
+        public void Reset()
+        {
+
+        }
+
+        #endregion
+    }
+}
