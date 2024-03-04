85ed4524860e47f10caf370851ad57405a101223
Peter Wagner <peterwagner@live.co.uk>
2024-03-04 07:43:30 +0000

    * Some tweaks

--- Source/Krypton Components/Krypton.Toolkit/Translations/Converters/ToastNotificationIconStrings.cs
+++ Source/Krypton Components/Krypton.Toolkit/Translations/Converters/ToastNotificationIconStrings.cs
@@ -54,7 +54,30 @@ namespace Krypton.Toolkit
         #region IsDefault
 
         [Browsable(false)]
-        public bool IsDefault => Application.Equals(DEFAULT_APPLICATION) &&
+        public bool IsDefault => !(ShouldSerializeApplication() ||
+                                   ShouldSerializeAsterisk() ||
+                                   ShouldSerializeCustom() ||
+                                   ShouldSerializeError() ||
+                                   ShouldSerializeExclamation() ||
+                                   ShouldSerializeHand() ||
+                                   ShouldSerializeInformation() ||
+                                   ShouldSerializeNone() ||
+                                   ShouldSerializeOk() ||
+                                   ShouldSerializeQuestion() ||
+                                   ShouldSerializeShield() ||
+                                   ShouldSerializeStop() ||
+                                   ShouldSerializeSystemApplication() ||
+                                   ShouldSerializeSystemAsterisk() ||
+                                   ShouldSerializeSystemError() ||
+                                   ShouldSerializeSystemExclamation() ||
+                                   ShouldSerializeSystemHand() ||
+                                   ShouldSerializeSystemInformation() ||
+                                   ShouldSerializeSystemQuestion() ||
+                                   ShouldSerializeSystemStop() ||
+                                   ShouldSerializeSystemWarning() ||
+                                   ShouldSerializeWarning() ||
+                                   ShouldSerializeWindowsLogo());
+                                  /*Application.Equals(DEFAULT_APPLICATION) &&
                                  Asterisk.Equals(DEFAULT_ASTERISK) &&
                                  Custom.Equals(DEFAULT_CUSTOM) &&
                                  Error.Equals(DEFAULT_ERROR) &&
@@ -76,7 +99,7 @@ namespace Krypton.Toolkit
                                  SystemStop.Equals(DEFAULT_SYSTEM_STOP) &&
                                  SystemWarning.Equals(DEFAULT_SYSTEM_WARNING) &&
                                  Warning.Equals(DEFAULT_SYSTEM_WARNING) &&
-                                 WindowsLogo.Equals(DEFAULT_WINDOWS_LOGO);
+                                 WindowsLogo.Equals(DEFAULT_WINDOWS_LOGO);*/
 
         #endregion
 
@@ -90,12 +113,20 @@ namespace Krypton.Toolkit
         [RefreshProperties(RefreshProperties.All)]
         public string Application { get; set; }
 
+        private bool ShouldSerializeApplication() => Application != DEFAULT_APPLICATION;
+
+        public void ResetApplication() => Application = DEFAULT_APPLICATION;
+
         /// <summary>Gets or sets the asterisk icon string.</summary>
         [Localizable(true)]
         [Category(@"Visuals")]
         [Description(@"Localised asterisk icon string.")]
         [DefaultValue(DEFAULT_ASTERISK)]
         public string Asterisk { get; set; }
+
+        private bool ShouldSerializeAsterisk() => Asterisk != DEFAULT_ASTERISK;
+
+        public void ResetAsterisk() => Asterisk = DEFAULT_ASTERISK;
 
         /// <summary>Gets or sets the custom icon string.</summary>
         [Localizable(true)]
@@ -104,12 +135,20 @@ namespace Krypton.Toolkit
         [DefaultValue(DEFAULT_CUSTOM)]
         public string Custom { get; set; }
 
+        private bool ShouldSerializeCustom() => Custom != DEFAULT_CUSTOM;
+
+        public void ResetCustom() => Custom = DEFAULT_CUSTOM;
+
         /// <summary>Gets or sets the error icon string.</summary>
         [Localizable(true)]
         [Category(@"Visuals")]
         [Description(@"Localised error icon string.")]
         [DefaultValue(DEFAULT_ERROR)]
         public string Error { get; set; }
+
+        private bool ShouldSerializeError() => Error != DEFAULT_ERROR;
+
+        public void ResetError() => Error = DEFAULT_ERROR;
 
         /// <summary>Gets or sets the exclamation icon string.</summary>
         [Localizable(true)]
@@ -118,12 +157,20 @@ namespace Krypton.Toolkit
         [DefaultValue(DEFAULT_EXCLAMATION)]
         public string Exclamation { get; set; }
 
+        private bool ShouldSerializeExclamation() => Exclamation != DEFAULT_EXCLAMATION;
+
+        public void ResetExclamation() => Exclamation = DEFAULT_EXCLAMATION;
+
         /// <summary>Gets or sets the hand icon string.</summary>
         [Localizable(true)]
         [Category(@"Visuals")]
         [Description(@"Localised hand icon string.")]
         [DefaultValue(DEFAULT_HAND)]
         public string Hand { get; set; }
+
+        private bool ShouldSerializeHand() => Hand != DEFAULT_HAND;
+
+        public void ResetHand() => Hand = DEFAULT_HAND;
 
         /// <summary>Gets or sets the information icon string.</summary>
         [Localizable(true)]
@@ -132,12 +179,20 @@ namespace Krypton.Toolkit
         [DefaultValue(DEFAULT_INFORMATION)]
         public string Information { get; set; }
 
+        private bool ShouldSerializeInformation() => Information != DEFAULT_INFORMATION;
+
+        public void ResetInformation() => Information = DEFAULT_INFORMATION;
+
         /// <summary>Gets or sets the none icon string.</summary>
         [Localizable(true)]
         [Category(@"Visuals")]
         [Description(@"Localised none icon string.")]
         [DefaultValue(DEFAULT_NONE)]
         public string None { get; set; }
+
+        private bool ShouldSerializeNone() => None != DEFAULT_NONE;
+
+        public void ResetNone() => None = DEFAULT_NONE;
 
         /// <summary>Gets or sets the ok icon string.</summary>
         [Localizable(true)]
@@ -146,12 +201,20 @@ namespace Krypton.Toolkit
         [DefaultValue(DEFAULT_OK)]
         public string Ok { get; set; }
 
+        private bool ShouldSerializeOk() => Ok != DEFAULT_OK;
+
+        public void ResetOk() => Ok = DEFAULT_OK;
+
         /// <summary>Gets or sets the question icon string.</summary>
         [Localizable(true)]
         [Category(@"Visuals")]
         [Description(@"Localised question icon string.")]
         [DefaultValue(DEFAULT_QUESTION)]
         public string Question { get; set; }
+
+        private bool ShouldSerializeQuestion() => Question != DEFAULT_QUESTION;
+
+        public void ResetQuestion() => Question = DEFAULT_QUESTION; 
 
         /// <summary>Gets or sets the shield icon string.</summary>
         [Localizable(true)]
@@ -160,12 +223,20 @@ namespace Krypton.Toolkit
         [DefaultValue(DEFAULT_SHIELD)]
         public string Shield { get; set; }
 
+        private bool ShouldSerializeShield() => Shield != DEFAULT_SHIELD;
+
+        public void ResetShield() => Shield = DEFAULT_SHIELD;
+
         /// <summary>Gets or sets the stop icon string.</summary>
         [Localizable(true)]
         [Category(@"Visuals")]
         [Description(@"Localised stop icon string.")]
         [DefaultValue(DEFAULT_STOP)]
         public string Stop { get; set; }
+
+        private bool ShouldSerializeStop() => Stop != DEFAULT_STOP;
+
+        public void ResetStop() => Stop = DEFAULT_STOP;
 
         /// <summary>Gets or sets the system application icon string.</summary>
         [Localizable(true)]
@@ -174,12 +245,20 @@ namespace Krypton.Toolkit
         [DefaultValue(DEFAULT_SYSTEM_APPLICATION)]
         public string SystemApplication { get; set; }
 
+        private bool ShouldSerializeSystemApplication() => SystemApplication != DEFAULT_SYSTEM_APPLICATION;
+
+        public void ResetSystemApplication() => SystemApplication = DEFAULT_SYSTEM_APPLICATION;
+
         /// <summary>Gets or sets the system asterisk icon string.</summary>
         [Localizable(true)]
         [Category(@"Visuals")]
         [Description(@"Localised system asterisk icon string.")]
         [DefaultValue(DEFAULT_SYSTEM_ASTERISK)]
         public string SystemAsterisk { get; set; }
+
+        private bool ShouldSerializeSystemAsterisk() => SystemAsterisk != DEFAULT_SYSTEM_ASTERISK;
+
+        public void ResetSystemAsterisk() => SystemAsterisk = DEFAULT_SYSTEM_ASTERISK;
 
         /// <summary>Gets or sets the system error icon string.</summary>
         [Localizable(true)]
@@ -188,12 +267,20 @@ namespace Krypton.Toolkit
         [DefaultValue(DEFAULT_SYSTEM_ERROR)]
         public string SystemError { get; set; }
 
+        private bool ShouldSerializeSystemError() => SystemError != DEFAULT_SYSTEM_ERROR;
+
+        public void ResetSystemError() => SystemError = DEFAULT_SYSTEM_ERROR;
+
         /// <summary>Gets or sets the system exclamation icon string.</summary>
         [Localizable(true)]
         [Category(@"Visuals")]
         [Description(@"Localised system exclamation icon string.")]
         [DefaultValue(DEFAULT_SYSTEM_EXCLAMATION)]
         public string SystemExclamation { get; set; }
+
+        private bool ShouldSerializeSystemExclamation() => SystemExclamation != DEFAULT_SYSTEM_EXCLAMATION;
+
+        public void ResetSystemExclamation() => SystemExclamation = DEFAULT_SYSTEM_EXCLAMATION;
 
         /// <summary>Gets or sets the system hand icon string.</summary>
         [Localizable(true)]
@@ -202,12 +289,20 @@ namespace Krypton.Toolkit
         [DefaultValue(DEFAULT_SYSTEM_HAND)]
         public string SystemHand { get; set; }
 
+        private bool ShouldSerializeSystemHand() => SystemHand != DEFAULT_SYSTEM_HAND;
+
+        public void ResetSystemHand() => SystemHand = DEFAULT_SYSTEM_HAND;
+
         /// <summary>Gets or sets the system information icon string.</summary>
         [Localizable(true)]
         [Category(@"Visuals")]
         [Description(@"Localised system information icon string.")]
         [DefaultValue(DEFAULT_SYSTEM_INFORMATION)]
         public string SystemInformation { get; set; }
+
+        private bool ShouldSerializeSystemInformation() => SystemInformation != DEFAULT_SYSTEM_INFORMATION;
+
+        public void ResetSystemInformation() => SystemInformation = DEFAULT_SYSTEM_INFORMATION;
 
         /// <summary>Gets or sets the system question icon string.</summary>
         [Localizable(true)]
@@ -216,12 +311,20 @@ namespace Krypton.Toolkit
         [DefaultValue(DEFAULT_SYSTEM_QUESTION)]
         public string SystemQuestion { get; set; }
 
+        private bool ShouldSerializeSystemQuestion() => SystemQuestion != DEFAULT_SYSTEM_QUESTION;
+
+        public void ResetSystemQuestion() => SystemQuestion = DEFAULT_SYSTEM_QUESTION;
+
         /// <summary>Gets or sets the system stop icon string.</summary>
         [Localizable(true)]
         [Category(@"Visuals")]
         [Description(@"Localised system stop icon string.")]
         [DefaultValue(DEFAULT_SYSTEM_STOP)]
         public string SystemStop { get; set; }
+
+        private bool ShouldSerializeSystemStop() => SystemStop != DEFAULT_SYSTEM_STOP;
+
+        public void ResetSystemStop() => SystemStop = DEFAULT_SYSTEM_STOP;
 
         /// <summary>Gets or sets the system warning icon string.</summary>
         [Localizable(true)]
@@ -230,12 +333,20 @@ namespace Krypton.Toolkit
         [DefaultValue(DEFAULT_SYSTEM_WARNING)]
         public string SystemWarning { get; set; }
 
+        private bool ShouldSerializeSystemWarning() => SystemWarning != DEFAULT_SYSTEM_WARNING;
+
+        public void ResetSystemWarning() => SystemWarning = DEFAULT_SYSTEM_WARNING;
+
         /// <summary>Gets or sets the warning icon string.</summary>
         [Localizable(true)]
         [Category(@"Visuals")]
         [Description(@"Localised warning icon string.")]
         [DefaultValue(DEFAULT_WARNING)]
         public string Warning { get; set; }
+
+        private bool ShouldSerializeWarning() => Warning != DEFAULT_WARNING;
+
+        public void ResetWarning() => Warning = DEFAULT_WARNING;
 
         /// <summary>Gets or sets the Windows logo icon string.</summary>
         [Localizable(true)]
@@ -244,57 +355,107 @@ namespace Krypton.Toolkit
         [DefaultValue(DEFAULT_WINDOWS_LOGO)]
         public string WindowsLogo { get; set; }
 
+        private bool ShouldSerializeWindowsLogo() => WindowsLogo != DEFAULT_WINDOWS_LOGO;
+
+        public void ResetWindowsLogo() => WindowsLogo = DEFAULT_WINDOWS_LOGO;
+
         #endregion
 
         #region Implementation
 
         public void Reset()
         {
-            Application = DEFAULT_APPLICATION;
+            //Application = DEFAULT_APPLICATION;
 
-            Asterisk = DEFAULT_ASTERISK;
+            //Asterisk = DEFAULT_ASTERISK;
 
-            Custom = DEFAULT_CUSTOM;
+            //Custom = DEFAULT_CUSTOM;
 
-            Error = DEFAULT_ERROR;
+            //Error = DEFAULT_ERROR;
 
-            Exclamation = DEFAULT_EXCLAMATION;
+            //Exclamation = DEFAULT_EXCLAMATION;
 
-            Hand = DEFAULT_HAND;
+            //Hand = DEFAULT_HAND;
 
-            Information = DEFAULT_INFORMATION;
+            //Information = DEFAULT_INFORMATION;
 
-            None = DEFAULT_NONE;
+            //None = DEFAULT_NONE;
 
-            Ok = DEFAULT_OK;
+            //Ok = DEFAULT_OK;
 
-            Question = DEFAULT_QUESTION;
+            //Question = DEFAULT_QUESTION;
 
-            Shield = DEFAULT_SHIELD;
+            //Shield = DEFAULT_SHIELD;
 
-            Stop = DEFAULT_STOP;
+            //Stop = DEFAULT_STOP;
 
-            SystemApplication = DEFAULT_SYSTEM_APPLICATION;
+            //SystemApplication = DEFAULT_SYSTEM_APPLICATION;
 
-            SystemAsterisk = DEFAULT_SYSTEM_ASTERISK;
+            //SystemAsterisk = DEFAULT_SYSTEM_ASTERISK;
 
-            SystemError = DEFAULT_SYSTEM_ERROR;
+            //SystemError = DEFAULT_SYSTEM_ERROR;
 
-            SystemExclamation = DEFAULT_SYSTEM_EXCLAMATION;
+            //SystemExclamation = DEFAULT_SYSTEM_EXCLAMATION;
 
-            SystemHand = DEFAULT_SYSTEM_HAND;
+            //SystemHand = DEFAULT_SYSTEM_HAND;
 
-            SystemInformation = DEFAULT_SYSTEM_INFORMATION;
+            //SystemInformation = DEFAULT_SYSTEM_INFORMATION;
 
-            SystemQuestion = DEFAULT_SYSTEM_QUESTION;
+            //SystemQuestion = DEFAULT_SYSTEM_QUESTION;
 
-            SystemStop = DEFAULT_SYSTEM_STOP;
+            //SystemStop = DEFAULT_SYSTEM_STOP;
 
-            SystemWarning = DEFAULT_SYSTEM_WARNING;
+            //SystemWarning = DEFAULT_SYSTEM_WARNING;
 
-            Warning = DEFAULT_WARNING;
+            //Warning = DEFAULT_WARNING;
 
-            WindowsLogo = DEFAULT_WINDOWS_LOGO;
+            //WindowsLogo = DEFAULT_WINDOWS_LOGO;
+
+            ResetApplication();
+
+            ResetAsterisk();
+
+            ResetCustom();
+
+            ResetError();
+
+            ResetExclamation();
+
+            ResetHand();
+
+            ResetInformation();
+
+            ResetNone();
+
+            ResetOk();
+
+            ResetQuestion();
+
+            ResetShield();
+
+            ResetStop();
+
+            ResetSystemApplication();
+
+            ResetSystemAsterisk();
+
+            ResetSystemError();
+
+            ResetSystemExclamation();
+
+            ResetSystemHand();
+
+            ResetSystemInformation();
+
+            ResetSystemQuestion();
+
+            ResetSystemStop();
+
+            ResetSystemWarning();
+
+            ResetWarning();
+
+            ResetWindowsLogo();
         }
 
         #endregion
