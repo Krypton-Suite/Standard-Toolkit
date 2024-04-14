73db7ced4d6d01143787ea69cee5d52cfb492b5f
Peter Wagner <peterwagner@live.co.uk>
2024-04-14 11:40:55 +0100

    Create KryptonToastNotificationUserInputController.cs

---
+++ Source/Krypton Components/Krypton.Toolkit/General/Toast Notification/KryptonToastNotificationUserInputController.cs
@@ -0,0 +1,45 @@
+﻿#region BSD License
+/*
+ *
+ *  New BSD 3-Clause License (https://github.com/Krypton-Suite/Standard-Toolkit/blob/master/LICENSE)
+ *  Modifications by Peter Wagner(aka Wagnerp) & Simon Coghlan(aka Smurf-IV), et al. 2024 - 2024. All rights reserved.
+ *
+ */
+#endregion
+
+namespace Krypton.Toolkit
+{
+    /// <summary>Manages the interaction between the <see cref="KryptonToastNotification"/> API and the Krypton toast notification user input types.</summary>
+    internal class KryptonToastNotificationUserInputController
+    {
+        #region Implementation
+
+        public static object ShowToast(KryptonUserInputToastNotificationData data)
+        {
+            switch (data.NotificationInputAreaType)
+            {
+                case KryptonToastNotificationInputAreaType.None:
+                    break;
+                case KryptonToastNotificationInputAreaType.ComboBox:
+                    break;
+                case KryptonToastNotificationInputAreaType.DomainUpDown:
+                    break;
+                case KryptonToastNotificationInputAreaType.NumericDropDown:
+                    break;
+                case KryptonToastNotificationInputAreaType.MaskedTextBox:
+                    break;
+                case KryptonToastNotificationInputAreaType.TextBox:
+                    break;
+                case null:
+                    break;
+                default:
+                    DebugTools.NotImplemented(data.ToString());
+                    break;
+            }
+
+            return new object();
+        }
+
+        #endregion
+    }
+}
