11e1f1d76972f7c96e0918894fca93bf5634c0da
Peter Wagner <peterwagner@live.co.uk>
2024-04-27 17:34:07 +0100

    * Some changes

---
+++ Source/Krypton Components/Krypton.Toolkit/Controls Toolkit/KryptonExpandableMessageBox.cs
@@ -0,0 +1,24 @@
+﻿#region BSD License
+/*
+ *
+ *  New BSD 3-Clause License (https://github.com/Krypton-Suite/Standard-Toolkit/blob/master/LICENSE)
+ *  Modifications by Peter Wagner(aka Wagnerp) & Simon Coghlan(aka Smurf-IV), et al. 2024 - 2024. All rights reserved.
+ *
+ */
+#endregion
+
+namespace Krypton.Toolkit
+{
+    public class KryptonExpandableMessageBox
+    {
+        #region Identity
+
+        /// <summary>Initializes a new instance of the <see cref="KryptonExpandableMessageBox" /> class.</summary>
+        public KryptonExpandableMessageBox()
+        {
+            DebugTools.NotImplemented(@"Coming soon...");
+        }
+
+        #endregion
+    }
+}
