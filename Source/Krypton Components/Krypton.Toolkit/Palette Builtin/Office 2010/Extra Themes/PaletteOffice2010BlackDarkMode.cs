a25e2bd4bc79c2e0ebec331ec0d7803d7caaf4fa
Peter Wagner <peterwagner@live.co.uk>
2024-06-12 09:52:57 +0100

    Revert "* Resolves  #1516"
    This reverts commit 93641f112df28e6ebe93c3ce0647b4b4f3917953.

--- Source/Krypton Components/Krypton.Toolkit/Palette Builtin/Office 2010/Extra Themes/PaletteOffice2010BlackDarkMode.cs
+++ Source/Krypton Components/Krypton.Toolkit/Palette Builtin/Office 2010/Extra Themes/PaletteOffice2010BlackDarkMode.cs
@@ -1611,7 +1611,8 @@ namespace Krypton.Toolkit
                 case PaletteBackStyle.ControlRibbonAppMenu:
                     return _ribbonColours[(int)SchemeOfficeColors.AppButtonBack2];
                 case PaletteBackStyle.ControlToolTip:
-                    return _toolTipBack2;
+                    // Note: It thinks that ToolTipBottom = 224 in the SchemeOfficeColors array, but it's assigned to 225?
+                    return _ribbonColours[225]; //_ribbonColours[(int)SchemeOfficeColors.ToolTipBottom];
                 case PaletteBackStyle.ContextMenuOuter:
                     return _contextMenuBack;
                 case PaletteBackStyle.ContextMenuSeparator:
