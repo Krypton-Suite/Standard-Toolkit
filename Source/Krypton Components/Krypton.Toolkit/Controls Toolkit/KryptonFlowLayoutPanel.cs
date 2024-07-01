4ab930fcf27e09ff48568ed246f88d73771d0a70
Peter Wagner <peterwagner@live.co.uk>
2024-07-01 11:57:05 +0100

    Create KryptonFlowLayoutPanel.cs

---
+++ Source/Krypton Components/Krypton.Toolkit/Controls Toolkit/KryptonFlowLayoutPanel.cs
@@ -0,0 +1,34 @@
+﻿#region BSD License
+/*
+ *
+ *  New BSD 3-Clause License (https://github.com/Krypton-Suite/Standard-Toolkit/blob/master/LICENSE)
+ *  Modifications by Peter Wagner(aka Wagnerp) & Simon Coghlan(aka Smurf-IV), et al. 2024 - 2024. All rights reserved.
+ *
+ */
+#endregion
+
+namespace Krypton.Toolkit
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [ToolboxItem(false)]
+    [DesignerCategory(@"code")]
+    internal class KryptonFlowLayoutPanel
+    {
+        #region Instance Fields
+
+
+
+        #endregion
+
+        #region Identity
+
+        public KryptonFlowLayoutPanel()
+        {
+            // ToDo: To be fleshed out in V100
+        }
+
+        #endregion
+    }
+}
