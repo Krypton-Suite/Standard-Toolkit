005016b79a5586f69b3e0df98eed61984286471d
Peter Wagner <peterwagner@live.co.uk>
2024-07-02 19:01:01 +0100

    * Tests

---
+++ Source/Krypton Components/TestForm/AboutBoxTest.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace TestForm
+{
+    partial class AboutBoxTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.kryptonPanel1 = new Krypton.Toolkit.KryptonPanel();
+            this.kryptonBorderEdge1 = new Krypton.Toolkit.KryptonBorderEdge();
+            this.kryptonButton1 = new Krypton.Toolkit.KryptonButton();
+            this.kbtnShow = new Krypton.Toolkit.KryptonButton();
+            this.kryptonPanel2 = new Krypton.Toolkit.KryptonPanel();
+            this.kryptonManager1 = new Krypton.Toolkit.KryptonManager(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel1)).BeginInit();
+            this.kryptonPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // kryptonPanel1
+            // 
+            this.kryptonPanel1.Controls.Add(this.kbtnShow);
+            this.kryptonPanel1.Controls.Add(this.kryptonButton1);
+            this.kryptonPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.kryptonPanel1.Location = new System.Drawing.Point(0, 400);
+            this.kryptonPanel1.Name = "kryptonPanel1";
+            this.kryptonPanel1.PanelBackStyle = Krypton.Toolkit.PaletteBackStyle.PanelAlternate;
+            this.kryptonPanel1.Size = new System.Drawing.Size(800, 50);
+            this.kryptonPanel1.TabIndex = 0;
+            // 
+            // kryptonBorderEdge1
+            // 
+            this.kryptonBorderEdge1.BorderStyle = Krypton.Toolkit.PaletteBorderStyle.HeaderPrimary;
+            this.kryptonBorderEdge1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.kryptonBorderEdge1.Location = new System.Drawing.Point(0, 399);
+            this.kryptonBorderEdge1.Name = "kryptonBorderEdge1";
+            this.kryptonBorderEdge1.Size = new System.Drawing.Size(800, 1);
+            this.kryptonBorderEdge1.Text = "kryptonBorderEdge1";
+            // 
+            // kryptonButton1
+            // 
+            this.kryptonButton1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.kryptonButton1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.kryptonButton1.Location = new System.Drawing.Point(698, 13);
+            this.kryptonButton1.Name = "kryptonButton1";
+            this.kryptonButton1.Size = new System.Drawing.Size(90, 25);
+            this.kryptonButton1.TabIndex = 0;
+            this.kryptonButton1.Values.DropDownArrowColor = System.Drawing.Color.Empty;
+            this.kryptonButton1.Values.Text = "Cance&l";
+            this.kryptonButton1.Values.UseAsADialogButton = true;
+            // 
+            // kbtnShow
+            // 
+            this.kbtnShow.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.kbtnShow.Location = new System.Drawing.Point(602, 13);
+            this.kbtnShow.Name = "kbtnShow";
+            this.kbtnShow.Size = new System.Drawing.Size(90, 25);
+            this.kbtnShow.TabIndex = 1;
+            this.kbtnShow.Values.DropDownArrowColor = System.Drawing.Color.Empty;
+            this.kbtnShow.Values.Text = "Show";
+            this.kbtnShow.Click += new System.EventHandler(this.kbtnShow_Click);
+            // 
+            // kryptonPanel2
+            // 
+            this.kryptonPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.kryptonPanel2.Location = new System.Drawing.Point(0, 0);
+            this.kryptonPanel2.Name = "kryptonPanel2";
+            this.kryptonPanel2.Size = new System.Drawing.Size(800, 399);
+            this.kryptonPanel2.TabIndex = 2;
+            // 
+            // kryptonManager1
+            // 
+            this.kryptonManager1.GlobalPaletteMode = Krypton.Toolkit.PaletteMode.ProfessionalSystem;
+            // 
+            // AboutBoxTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.kryptonPanel2);
+            this.Controls.Add(this.kryptonBorderEdge1);
+            this.Controls.Add(this.kryptonPanel1);
+            this.Name = "AboutBoxTest";
+            this.Text = "AboutBoxTest";
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel1)).EndInit();
+            this.kryptonPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private KryptonPanel kryptonPanel1;
+        private KryptonBorderEdge kryptonBorderEdge1;
+        private KryptonButton kryptonButton1;
+        private KryptonButton kbtnShow;
+        private KryptonPanel kryptonPanel2;
+        private KryptonManager kryptonManager1;
+    }
+}
---
+++ Source/Krypton Components/TestForm/AboutBoxTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestForm
+{
+    public partial class AboutBoxTest : KryptonForm
+    {
+        public AboutBoxTest()
+        {
+            InitializeComponent();
+        }
+
+        private void kbtnShow_Click(object sender, EventArgs e)
+        {
+            KryptonAboutBoxData aboutBoxData = new KryptonAboutBoxData()
+            {
+                ApplicationName = @"TestForm",
+                CurrentAssembly = Assembly.GetExecutingAssembly(),
+                HeaderImage = null,
+                MainImage = null,
+                ShowToolkitInformation = false,
+                UseFullBuiltOnDate = false
+            };
+
+            KryptonAboutToolkitData aboutToolkitData = new KryptonAboutToolkitData();
+
+
+        }
+    }
+}
