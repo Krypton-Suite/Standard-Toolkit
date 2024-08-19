5d5b37935de0df4e9cca3eb61c72d65af9ca9a29
Giduac <tweakdummy@protonmail.com>
2024-08-19 15:33:59 +0200

    1692-GIDUAC-bug-inputtext-should-not-be-using-textlabelcontrol-colours Corrects KMessageBoxesKRTBox text color

--- Source/Krypton Components/Krypton.Toolkit/General/GlobalStaticValues.cs
+++ Source/Krypton Components/Krypton.Toolkit/General/GlobalStaticValues.cs
@@ -372,6 +372,18 @@ namespace Krypton.Toolkit
 
         public static readonly string DEFAULT_LONG_SEED_TEXT = $"BSD 3-Clause License\r\n\r\nCopyright (c) 2017 - {DateTime.Now.Year}, Krypton Suite\r\n\r\nAll rights reserved.\r\n\r\nRedistribution and use in source and binary forms, with or without\r\nmodification, are permitted provided that the following conditions are met:\r\n\r\n1. Redistributions of source code must retain the above copyright notice, this\r\n   list of conditions and the following disclaimer.\r\n\r\n2. Redistributions in binary form must reproduce the above copyright notice,\r\n   this list of conditions and the following disclaimer in the documentation\r\n   and/or other materials provided with the distribution.\r\n\r\n3. Neither the name of the copyright holder nor the names of its\r\n   contributors may be used to endorse or promote products derived from\r\n   this software without specific prior written permission.\r\n\r\nTHIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\"\r\nAND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE\r\nIMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE\r\nDISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE\r\nFOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL\r\nDAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR\r\nSERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER\r\nCAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,\r\nOR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE\r\nOF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.";
 
+        #region Properties
+        /// <summary> 
+        /// KryptonMessageBoxes that use the KRichtTextBox need another color for the text.<br/>
+        /// Set the text colour to the one a non-input control uses.
+        /// </summary>
+        public static Color KryptonMessageBoxRichTextBoxTextColor 
+        {
+            // per ticket #1692
+            get => KryptonManager.CurrentGlobalPalette.GetContentLongTextColor1(PaletteContentStyle.LabelNormalPanel, PaletteState.Normal);
+        }
+        #endregion
+
         #region Methods
         /// <summary>
         /// Helper method that returns a generic message when a variable is null.
