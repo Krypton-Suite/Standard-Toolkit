c7499ffb8dc416797696b8831c9473cdceb5d92e
Peter Wagner <peterwagner@live.co.uk>
2024-08-30 18:54:57 +0100

    * Fix black theme text colours

---
+++ Source/Krypton Components/TestForm/NavigatorTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestForm
+{
+    public partial class NavigatorTest : KryptonForm
+    {
+        public NavigatorTest()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSpecExpandCollapse_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
