a69247ae1b079cb4c490915d6b70199fc19014a8
Peter Wagner <peterwagner@live.co.uk>
2024-11-05 18:57:51 +0000

    * Licenses

--- Source/Krypton Components/TestForm/CheckBoxStyleExamples.cs
+++ Source/Krypton Components/TestForm/CheckBoxStyleExamples.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿#region BSD License
+/*
+ *
+ *  New BSD 3-Clause License (https://github.com/Krypton-Suite/Standard-Toolkit/blob/master/LICENSE)
+ *  Modifications by Peter Wagner(aka Wagnerp) & Simon Coghlan(aka Smurf-IV), et al. 2024 - 2025. All rights reserved.
+ *
+ */
+#endregion
 
 namespace TestForm
 {
--- Source/Krypton Components/TestForm/SplashScreenExample.cs
+++ Source/Krypton Components/TestForm/SplashScreenExample.cs
@@ -1,14 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+﻿#region BSD License
+/*
+ *
+ *  New BSD 3-Clause License (https://github.com/Krypton-Suite/Standard-Toolkit/blob/master/LICENSE)
+ *  Modifications by Peter Wagner(aka Wagnerp) & Simon Coghlan(aka Smurf-IV), et al. 2024 - 2025. All rights reserved.
+ *
+ */
+#endregion
+
 using System.IO;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace TestForm
 {
