b315d232ff5997fd041a89155283793c5b0fddf2
Peter Wagner <peterwagner@live.co.uk>
2025-03-03 14:54:02 +0000

    * Stash changes

---
+++ Source/Krypton Components/Krypton.Toolkit/Utilities/KryptonTheme.cs
@@ -0,0 +1,38 @@
+﻿namespace Krypton.Toolkit
+{
+    /// <summary>
+    /// Represents a Krypton theme with metadata and style properties.
+    /// </summary>
+    public class KryptonTheme
+    {
+        /// <summary>
+        /// The internal palette mode name corresponding to the theme.
+        /// </summary>
+        public string BasePaletteMode { get; set; } = "Office 2007";
+
+        /// <summary>
+        /// A human-readable name for the theme.
+        /// </summary>
+        public string DisplayName { get; set; } = "Office 2007";
+
+        /// <summary>
+        /// The hotkey prefix setting that determines how shortcut keys are displayed.
+        /// </summary>
+        public string HotKeyPrefix { get; set; } = "None";
+
+        /// <summary>
+        /// A dictionary storing the color mappings for the theme.
+        /// </summary>
+        public Dictionary<string, Color> Colors { get; set; } = new();
+
+        /// <summary>
+        /// A dictionary storing embedded images for the theme.
+        /// </summary>
+        public Dictionary<string, Image> Images { get; set; } = new();
+
+        /// <summary>
+        /// A dictionary storing font data in a serialized format.
+        /// </summary>
+        public Dictionary<string, byte[]> Fonts { get; set; } = new();
+    }
+}
---
+++ Source/Krypton Components/Krypton.Toolkit/Utilities/KryptonThemeLoader.cs
@@ -0,0 +1,70 @@
+﻿#region BSD License
+/*
+ *
+ *  New BSD 3-Clause License (https://github.com/Krypton-Suite/Standard-Toolkit/blob/master/LICENSE)
+ *  Modifications by Peter Wagner(aka Wagnerp), Simon Coghlan(aka Smurf-IV) & Giduac, et al. 2025 - 2025. All rights reserved.
+ *
+ */
+#endregion
+
+namespace Krypton.Toolkit
+{
+    public static class KryptonThemeLoader
+    {
+        public static List<string> GetAvailableThemes()
+        {
+            Assembly assembly = Assembly.Load("Krypton.Base.Palettes");
+            List<string> themes = new List<string>();
+
+            foreach (var resource in assembly.GetManifestResourceNames())
+            {
+                if (resource.EndsWith(".ktheme"))
+                {
+                    string themeName = resource.Replace($"{GlobalStaticValues.EMBEDDED_THEME_NAMESPACE}.", "").Replace(".ktheme", "");
+                    themes.Add(themeName);
+                }
+            }
+
+            return themes;
+        }
+
+        public static KryptonTheme LoadEmbeddedTheme(string themeName)
+        {
+            Assembly assembly = Assembly.Load("Krypton.Base.Palettes");
+            string resourceName = $"{GlobalStaticValues.EMBEDDED_THEME_NAMESPACE}.{themeName}.ktheme";
+
+            using (Stream? stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                if (stream == null)
+                {
+                    throw new FileNotFoundException($"Theme {themeName} not found in Krypton.Base.Palettes.dll");
+                }
+
+                XmlDocument doc = new XmlDocument();
+                doc.Load(stream);
+                return ParseTheme(doc);
+            }
+        }
+
+        private static KryptonTheme ParseTheme(XmlDocument doc)
+        {
+            KryptonTheme theme = new KryptonTheme();
+            XmlElement? root = doc.DocumentElement;
+
+            if (root == null)
+            {
+                throw new Exception("Invalid theme format.");
+            }
+
+            // Extract BasePaletteMode
+            XmlNode? basePaletteNode = root.SelectSingleNode("BasePaletteMode");
+            theme.BasePaletteMode = basePaletteNode?.Attributes?["Value"]?.Value ?? "Unknown";
+
+            // Extract Display Name
+            XmlNode? displayNameNode = root.SelectSingleNode("DisplayName");
+            theme.DisplayName = displayNameNode?.InnerText ?? theme.BasePaletteMode;
+
+            return theme;
+        }
+    }
+}
