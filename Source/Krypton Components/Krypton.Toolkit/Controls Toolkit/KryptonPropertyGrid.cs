addc15b74b4f260e6511ec8a1ea4f2235fed85f7
Peter Wagner <peterwagner@live.co.uk>
2025-04-05 07:58:24 +0100

    * Resolves #2165 for V100

--- Source/Krypton Components/Krypton.Toolkit/Controls Toolkit/KryptonPropertyGrid.cs
+++ Source/Krypton Components/Krypton.Toolkit/Controls Toolkit/KryptonPropertyGrid.cs
@@ -233,12 +233,21 @@ namespace Krypton.Toolkit
         #endregion
 
         #region Events
+        
+        public new event PreviewKeyDownEventHandler? PreviewKeyDown;
 
-        // TODO:
+        public event EventHandler? PropertySortChanged;
+
+        public event PropertyTabChangedEventHandler? PropertyTabChanged;
+
+        //public event PropertyChangingEventHandler? PropertyChanging;
+
+        public event PropertyValueChangedEventHandler? PropertyValueChanged;
+
 
         #endregion
 
-        #region Constructor
+        #region Identity
 
         /// <summary>Initializes a new instance of the <see cref="KryptonPropertyGrid" /> class.</summary>
         public KryptonPropertyGrid()
@@ -262,6 +271,11 @@ namespace Krypton.Toolkit
             _propertyGrid.Click += OnPropertyGridClick; // SKC: make sure that the default click is also routed.
             _propertyGrid.GotFocus += OnPropertyGridGotFocus;
             _propertyGrid.LostFocus += OnPropertyGridLostFocus;
+            _propertyGrid.PreviewKeyDown += OnPreviewKeyDown;
+            _propertyGrid.PropertySortChanged += OnPropertySortChanged;
+            _propertyGrid.PropertyTabChanged += OnPropertyTabChanged;
+            //_propertyGrid.PropertyChanging += OnPropertyChanging;
+            _propertyGrid.PropertyValueChanged += OnPropertyValueChanged;
 
             _layoutFill = new ViewLayoutFill(_propertyGrid)
             {
@@ -864,6 +878,14 @@ namespace Krypton.Toolkit
             PerformNeedPaint(false);
             _propertyGrid.Invalidate();
         }
+
+        private void OnPreviewKeyDown(object? sender, PreviewKeyDownEventArgs e) => PreviewKeyDown?.Invoke(sender, e);
+
+        private void OnPropertySortChanged(object? sender, EventArgs e) => PropertySortChanged?.Invoke(sender, e);
+
+        private void OnPropertyTabChanged(object? sender, PropertyTabChangedEventArgs e) => PropertyTabChanged?.Invoke(sender, e);
+
+        private void OnPropertyValueChanged(object? sender, PropertyValueChangedEventArgs e) => PropertyValueChanged?.Invoke(sender, e);
 
         /// <inheritdoc />
         protected override void OnEnabledChanged(EventArgs e)
