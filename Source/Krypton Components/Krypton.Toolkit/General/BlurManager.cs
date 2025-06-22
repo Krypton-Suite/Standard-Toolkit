eaafa9974b0054ace6eba63422e8daefe517025d
Peter Wagner <peterwagner@live.co.uk>
2025-06-22 12:29:57 +0100

    * Addressed feedback

--- Source/Krypton Components/Krypton.Toolkit/General/BlurManager.cs
+++ Source/Krypton Components/Krypton.Toolkit/General/BlurManager.cs
@@ -167,7 +167,7 @@ namespace Krypton.Toolkit
             if (!_blurValues.BlurWhenFocusLost
                 || _parentForm.IsDisposed
                 || _parentForm.Disposing
-                || DesignModeHelper.IsInDesignMode()
+                || DesignModeHelper.IsInDesignMode
                 )
             {
                 // Has blur been turned off ?
