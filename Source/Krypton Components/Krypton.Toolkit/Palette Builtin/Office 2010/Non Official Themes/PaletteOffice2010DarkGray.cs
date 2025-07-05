7cc600321f6d0c83438b270cbc919ac75ed5fa9a
Lesandro Gotardo <148582027+lesandrog@users.noreply.github.com>
2025-07-05 16:50:39 -0300

    Update PaletteOffice2010DarkGray.cs

--- Source/Krypton Components/Krypton.Toolkit/Palette Builtin/Office 2010/Non Official Themes/PaletteOffice2010DarkGray.cs
+++ Source/Krypton Components/Krypton.Toolkit/Palette Builtin/Office 2010/Non Official Themes/PaletteOffice2010DarkGray.cs
@@ -272,7 +272,6 @@ namespace Krypton.Toolkit
             Color.FromArgb(240, 241, 242), // RibbonGalleryBackTracking
             Color.FromArgb(195, 200, 209), // RibbonGalleryBack1
             Color.FromArgb(217, 220, 224), // RibbonGalleryBack2
-            Color.Empty, // RibbonTabTracking1
             Color.Empty, // RibbonTabTracking3
             Color.Empty, // RibbonTabTracking4
             Color.Empty, // RibbonGroupBorder3
