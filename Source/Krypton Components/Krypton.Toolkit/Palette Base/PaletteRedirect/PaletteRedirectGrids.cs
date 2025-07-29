681bece74336e2be1964e28503d73cda68e4402a
tobitege <10787084+tobitege@users.noreply.github.com>
2025-07-29 07:23:32 +0200

    Fix exception in PaletteRedirectGrids.GetInheritBack for BoldedOverride

--- Source/Krypton Components/Krypton.Toolkit/Palette Base/PaletteRedirect/PaletteRedirectGrids.cs
+++ Source/Krypton Components/Krypton.Toolkit/Palette Base/PaletteRedirect/PaletteRedirectGrids.cs
@@ -1,12 +1,12 @@
 ﻿#region BSD License
 /*
- * 
+ *
  * Original BSD 3-Clause License (https://github.com/ComponentFactory/Krypton/blob/master/LICENSE)
  *  © Component Factory Pty Ltd, 2006 - 2016, (Version 4.5.0.0) All rights reserved.
- * 
+ *
  *  New BSD 3-Clause License (https://github.com/Krypton-Suite/Standard-Toolkit/blob/master/LICENSE)
  *  Modifications by Peter Wagner (aka Wagnerp), Simon Coghlan (aka Smurf-IV), Giduac & Ahmed Abdelhameed et al. 2017 - 2025. All rights reserved.
- *  
+ *
  */
 #endregion
 
@@ -833,7 +833,7 @@ public class PaletteRedirectGrids : PaletteRedirect
     {
         IPaletteContent inherit = GetInheritContent(style, state);
 
-        return inherit?.GetBorderContentPadding(owningForm, state) 
+        return inherit?.GetBorderContentPadding(owningForm, state)
                ?? Target!.GetBorderContentPadding(owningForm, style, state);
     }
 
@@ -847,7 +847,7 @@ public class PaletteRedirectGrids : PaletteRedirect
     {
         IPaletteContent inherit = GetInheritContent(style, state);
 
-        return inherit?.GetContentAdjacentGap(state) 
+        return inherit?.GetContentAdjacentGap(state)
                ?? Target!.GetContentAdjacentGap(style, state);
     }
     #endregion
@@ -887,6 +887,7 @@ public class PaletteRedirectGrids : PaletteRedirect
                 }
                 break;
 
+            case PaletteState.BoldedOverride:
             case PaletteState.Normal:
                 switch (style)
                 {
