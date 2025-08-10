97c20a07b99b8814dbf56e9f79223d3b34c82dac
Giduac <tweakdummy@protonmail.com>
2025-08-10 15:44:48 +0200

    2376-V100-implement-outlookgrid Update license headers

--- Source/Krypton Components/Krypton.Toolkit/KryptonOutlookGrid/FilterBuilder/KryptonOutlookGridFilter.designer.cs
+++ Source/Krypton Components/Krypton.Toolkit/KryptonOutlookGrid/FilterBuilder/KryptonOutlookGridFilter.designer.cs
@@ -1,4 +1,23 @@
-﻿namespace Krypton.Toolkit;
+#region Licences
+/*
+ *
+ *  New BSD 3-Clause License (https://github.com/Krypton-Suite/Standard-Toolkit/blob/master/LICENSE)
+ *  Modifications by Peter Wagner (aka Wagnerp), Simon Coghlan (aka Smurf-IV), Giduac, tobitege & Ahmed Abdelhameed et al. 2024 - 2025. All rights reserved.
+ *
+ */
+
+//--------------------------------------------------------------------------------
+// Copyright (C) 2013-2021 JDH Software - <support@jdhsoftware.com>
+//
+// This program is provided to you under the terms of the Microsoft Public
+// License (Ms-PL) as published at https://github.com/Cocotteseb/Krypton-OutlookGrid/blob/master/LICENSE.md
+//
+// Visit https://www.jdhsoftware.com and follow @jdhsoftware on Twitter
+//
+//--------------------------------------------------------------------------------
+#endregion
+
+namespace Krypton.Toolkit;
 
 partial class KryptonOutlookGridFilter
 {
