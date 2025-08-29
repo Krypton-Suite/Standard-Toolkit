1e244c7b11b7b41d83bf8fc65f13a83519368edf
Giduac <tweakdummy@protonmail.com>
2025-08-29 09:32:52 +0200

    2463-V95-KryptonForm-Titlebar-control-buttons-issues - Correct Buttonspecs and FormBorderStyle behaviour - And the changelog

--- Source/Krypton Components/Krypton.Toolkit/ButtonSpec/ButtonSpecFormWindowClose.cs
+++ Source/Krypton Components/Krypton.Toolkit/ButtonSpec/ButtonSpecFormWindowClose.cs
@@ -5,7 +5,7 @@
  *  © Component Factory Pty Ltd, 2006 - 2016, (Version 4.5.0.0) All rights reserved.
  * 
  *  New BSD 3-Clause License (https://github.com/Krypton-Suite/Standard-Toolkit/blob/master/LICENSE)
- *  Modifications by Peter Wagner (aka Wagnerp), Simon Coghlan (aka Smurf-IV), Giduac & Ahmed Abdelhameed et al. 2017 - 2024. All rights reserved.
+ *  Modifications by Peter Wagner (aka Wagnerp), Simon Coghlan (aka Smurf-IV), Giduac & Ahmed Abdelhameed et al. 2017 - 2025. All rights reserved.
  *  
  */
 #endregion
@@ -60,11 +60,8 @@ namespace Krypton.Toolkit
         /// </summary>
         /// <param name="palette">Palette to use for inheriting values.</param>
         /// <returns>Button visibility.</returns>
-        public override bool GetVisible(PaletteBase palette)
-        {
-            // Have all buttons been turned off?
-            return KryptonForm is { ControlBox: true, CloseBox: true };
-        }
+        public override bool GetVisible(PaletteBase palette) =>
+            KryptonForm.CloseBox;
 
         /// <summary>
         /// Gets the button enabled state.
