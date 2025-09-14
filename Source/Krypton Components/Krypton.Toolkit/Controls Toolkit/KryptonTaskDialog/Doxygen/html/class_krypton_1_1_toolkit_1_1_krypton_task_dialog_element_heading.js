6f52c03ac5d87246f733ac336de94318cc540aae
Giduac <tweakdummy@protonmail.com>
2025-09-14 16:16:58 +0200

    1376-V100-Extending-KryptonTaskDialog - Updates Doxygen documentation

--- Source/Krypton Components/Krypton.Toolkit/Controls Toolkit/KryptonTaskDialog/Doxygen/html/class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_heading.js
+++ Source/Krypton Components/Krypton.Toolkit/Controls Toolkit/KryptonTaskDialog/Doxygen/html/class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_heading.js
@@ -1,7 +1,7 @@
 var class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_heading =
 [
-    [ "KryptonTaskDialogElementHeading", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_heading.html#a4da5dc8e79d4936e79d674920b365fa9", null ],
-    [ "ToString", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_heading.html#a6f329257134cf20b4e8d819c8e325984", null ],
+    [ "KryptonTaskDialogElementHeading", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_heading.html#afd39428c0782e9a0c0da0a2ea430d064", null ],
+    [ "Dispose", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_heading.html#aa11b1564f5e00a221c84643f8936de22", null ],
     [ "BackColor1", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_heading.html#aa10baf813eac3b6341def3da09cccdaf", null ],
     [ "BackColor2", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_heading.html#abc9d68b9690957c1995b2b8caa0d6832", null ],
     [ "ForeColor", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_heading.html#a758b153d93c40918f183bf8c7b6b6b3e", null ],
--- Source/Krypton Components/Krypton.Toolkit/Controls Toolkit/KryptonTaskDialog/Doxygen/html/hierarchy.js
+++ Source/Krypton Components/Krypton.Toolkit/Controls Toolkit/KryptonTaskDialog/Doxygen/html/hierarchy.js
@@ -7,8 +7,15 @@ var hierarchy =
       [ "Krypton.Toolkit.KryptonTaskDialogElementBase", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_base.html", [
         [ "Krypton.Toolkit.KryptonTaskDialogElementCommonButtons", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_common_buttons.html", null ],
         [ "Krypton.Toolkit.KryptonTaskDialogElementContent", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_content.html", null ],
+        [ "Krypton.Toolkit.KryptonTaskDialogElementContentTest", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_content_test.html", null ],
         [ "Krypton.Toolkit.KryptonTaskDialogElementFooterBar", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_footer_bar.html", null ],
-        [ "Krypton.Toolkit.KryptonTaskDialogElementHeading", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_heading.html", null ]
+        [ "Krypton.Toolkit.KryptonTaskDialogElementHeading", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_heading.html", null ],
+        [ "Krypton.Toolkit.KryptonTaskDialogElementSingleLineControlBase", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_single_line_control_base.html", [
+          [ "Krypton.Toolkit.KryptonTaskDialogElementCheckBox", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_check_box.html", null ],
+          [ "Krypton.Toolkit.KryptonTaskDialogElementComboBox", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_combo_box.html", null ],
+          [ "Krypton.Toolkit.KryptonTaskDialogElementHyperLink", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_hyper_link.html", null ],
+          [ "Krypton.Toolkit.KryptonTaskDialogElementProgresBar", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_progres_bar.html", null ]
+        ] ]
       ] ],
       [ "Krypton.Toolkit.KryptonTaskDialogIconController", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_icon_controller.html", null ]
     ] ],
@@ -24,8 +31,15 @@ var hierarchy =
     [ "Krypton.Toolkit.IKryptonTaskDialogElementContent", "interface_krypton_1_1_toolkit_1_1_i_krypton_task_dialog_element_content.html", [
       [ "Krypton.Toolkit.KryptonTaskDialogElementContent", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_content.html", null ]
     ] ],
+    [ "Krypton.Toolkit.IKryptonTaskDialogElementDescription", "interface_krypton_1_1_toolkit_1_1_i_krypton_task_dialog_element_description.html", [
+      [ "Krypton.Toolkit.KryptonTaskDialogElementComboBox", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_combo_box.html", null ],
+      [ "Krypton.Toolkit.KryptonTaskDialogElementContentTest", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_content_test.html", null ],
+      [ "Krypton.Toolkit.KryptonTaskDialogElementHyperLink", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_hyper_link.html", null ],
+      [ "Krypton.Toolkit.KryptonTaskDialogElementProgresBar", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_progres_bar.html", null ]
+    ] ],
     [ "Krypton.Toolkit.IKryptonTaskDialogElementEventSizeChanged", "interface_krypton_1_1_toolkit_1_1_i_krypton_task_dialog_element_event_size_changed.html", [
-      [ "Krypton.Toolkit.KryptonTaskDialogElementContent", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_content.html", null ]
+      [ "Krypton.Toolkit.KryptonTaskDialogElementBase", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_base.html", null ],
+      [ "Krypton.Toolkit.KryptonTaskDialogElementContentTest", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_content_test.html", null ]
     ] ],
     [ "Krypton.Toolkit.IKryptonTaskDialogElementFont", "interface_krypton_1_1_toolkit_1_1_i_krypton_task_dialog_element_font.html", null ],
     [ "Krypton.Toolkit.IKryptonTaskDialogElementFooterBar", "interface_krypton_1_1_toolkit_1_1_i_krypton_task_dialog_element_footer_bar.html", [
@@ -33,22 +47,32 @@ var hierarchy =
     ] ],
     [ "Krypton.Toolkit.IKryptonTaskDialogElementForeColor", "interface_krypton_1_1_toolkit_1_1_i_krypton_task_dialog_element_fore_color.html", [
       [ "Krypton.Toolkit.KryptonTaskDialogElementContent", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_content.html", null ],
+      [ "Krypton.Toolkit.KryptonTaskDialogElementContentTest", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_content_test.html", null ],
       [ "Krypton.Toolkit.KryptonTaskDialogElementFooterBar", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_footer_bar.html", null ],
-      [ "Krypton.Toolkit.KryptonTaskDialogElementHeading", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_heading.html", null ]
+      [ "Krypton.Toolkit.KryptonTaskDialogElementHeading", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_heading.html", null ],
+      [ "Krypton.Toolkit.KryptonTaskDialogElementSingleLineControlBase", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_single_line_control_base.html", null ]
     ] ],
     [ "Krypton.Toolkit.IKryptonTaskDialogElementIconType", "interface_krypton_1_1_toolkit_1_1_i_krypton_task_dialog_element_icon_type.html", [
       [ "Krypton.Toolkit.KryptonTaskDialogElementFooterBar", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_footer_bar.html", null ],
       [ "Krypton.Toolkit.KryptonTaskDialogElementHeading", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_heading.html", null ]
     ] ],
     [ "Krypton.Toolkit.IKryptonTaskDialogElementText", "interface_krypton_1_1_toolkit_1_1_i_krypton_task_dialog_element_text.html", [
+      [ "Krypton.Toolkit.KryptonTaskDialogElementCheckBox", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_check_box.html", null ],
+      [ "Krypton.Toolkit.KryptonTaskDialogElementContentTest", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_content_test.html", null ],
       [ "Krypton.Toolkit.KryptonTaskDialogElementHeading", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_heading.html", null ]
     ] ],
     [ "Krypton.Toolkit.IKryptonTaskDialogElementTextAlignmentHorizontal", "interface_krypton_1_1_toolkit_1_1_i_krypton_task_dialog_element_text_alignment_horizontal.html", [
       [ "Krypton.Toolkit.KryptonTaskDialogElementHeading", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_heading.html", null ]
     ] ],
     [ "Krypton.Toolkit.IKryptonTaskDialogElementTextAlignmentVertical", "interface_krypton_1_1_toolkit_1_1_i_krypton_task_dialog_element_text_alignment_vertical.html", null ],
+    [ "Krypton.Toolkit.IKryptonTaskDialogElementUrl", "interface_krypton_1_1_toolkit_1_1_i_krypton_task_dialog_element_url.html", [
+      [ "Krypton.Toolkit.KryptonTaskDialogElementHyperLink", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_hyper_link.html", null ]
+    ] ],
     [ "KryptonForm", null, [
       [ "Krypton.Toolkit.KryptonTaskDialogKryptonForm", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_krypton_form.html", null ]
     ] ],
+    [ "Krypton.Toolkit.KryptonTaskDialogDefaults", "struct_krypton_1_1_toolkit_1_1_krypton_task_dialog_defaults.html", null ],
+    [ "Krypton.Toolkit.KryptonTaskDialogDefaultsTLP", "struct_krypton_1_1_toolkit_1_1_krypton_task_dialog_defaults_t_l_p.html", null ],
+    [ "Krypton.Toolkit.KryptonTaskDialogElementProgresBarProperties", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_progres_bar_properties.html", null ],
     [ "Krypton.Toolkit.KryptonTaskDialogFormProperties", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_form_properties.html", null ]
 ];
