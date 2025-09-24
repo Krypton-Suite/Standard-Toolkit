a80a2fd18835711f5e2f4e52519dbb15061a3233
giduac <96108132+giduac@users.noreply.github.com>
2025-09-24 17:17:38 +0200

    1376-V100-Extending-KryptonTaskDialog (#2518)
    * 1376-V100-Extending-KryptonTaskDialog
    - Removes CommonButtons
    - Integrates CommonButtons in the FooterBar
    - Adds a separator to each element which can toggled.
    - Some housekeeping.
    
    * 1376-V100-Extending-KryptonTaskDialog
    - Removes CommonButtons
    - Integrates CommonButtons in the FooterBar
    - Adds a separator to each element which can toggled.
    - Some housekeeping.
    
    * 1376-V100-Extending-KryptonTaskDialog
    - Removes CommonButtons
    - Integrates CommonButtons in the FooterBar
    - Adds a separator to each element which can toggled.
    - Some housekeeping.
    
    * 1376-V100-Extending-KryptonTaskDialog
    - Removes CommonButtons
    - Integrates CommonButtons in the FooterBar
    - Adds a separator to each element which can toggled.
    - Some housekeeping.

---
+++ Source/Krypton Components/Krypton.Toolkit/Controls Toolkit/KryptonTaskDialog/Elements/KryptonTaskDialogElementFooterBarCommonButtonProperties.cs
@@ -0,0 +1,113 @@
+#region BSD License
+/*
+ *
+ * New BSD 3-Clause License (https://github.com/Krypton-Suite/Standard-Toolkit/blob/master/LICENSE)
+ * Modifications by Peter Wagner (aka Wagnerp), Simon Coghlan (aka Smurf-IV), Giduac, Ahmed Abdelhameed, tobitege et al. 2025 - 2025. All rights reserved.
+ *
+ */
+#endregion
+
+namespace Krypton.Toolkit;
+
+/// <summary>
+/// Used internally by KryptonTaskDialogElementFooterBar
+/// </summary>
+public class KryptonTaskDialogElementFooterBarCommonButtonProperties 
+{
+    #region Fields
+    private KryptonTaskDialogElementFooterBar _footerBar;
+    private List<KryptonButton> _buttons;
+    private Action<bool> _onSizeChanged;
+    private KryptonTaskDialogCommonButtonTypes _commonButtons;
+    #endregion
+
+    #region Identity
+    public KryptonTaskDialogElementFooterBarCommonButtonProperties(KryptonTaskDialogElementFooterBar footerBar, 
+        List<KryptonButton> buttons, Action<bool> onSizeChanged)
+    {
+        _footerBar = footerBar;
+        _buttons = buttons;
+        _onSizeChanged = onSizeChanged;
+        _commonButtons = KryptonTaskDialogCommonButtonTypes.None;
+    }
+    #endregion
+
+    #region Public
+    /// <summary>
+    /// Rounds the button corners
+    /// </summary>
+    public bool RoundedCorners
+    {
+        get => field;
+
+        set
+        {
+            if (field != value)
+            {
+                field = value;
+
+                int rounding = value ? 10 : -1;
+                _buttons.ForEach(b => b.StateCommon.Border.Rounding = rounding);
+                _footerBar.LayoutDirty = true;
+                _onSizeChanged(false);
+            }
+        }
+    }
+
+    /// <inheritdoc/>
+    public KryptonTaskDialogCommonButtonTypes Buttons
+    {
+        get => _commonButtons;
+
+        set
+        {
+            if (_commonButtons != value)
+            {
+                _commonButtons = value;
+                _footerBar.OnCommonButtonsChanged();
+            }
+        }
+    }
+
+    /// <inheritdoc/>
+    public KryptonTaskDialogCommonButtonTypes AcceptButton
+    {
+        get => field;
+
+        set
+        {
+            if (field != value)
+            {
+                field = value;
+                _footerBar.OnCommonButtonsChanged();
+            }
+        }
+    }
+
+    /// <inheritdoc/>
+    public KryptonTaskDialogCommonButtonTypes CancelButton
+    {
+        get => field;
+
+        set
+        {
+            if (field != value)
+            {
+                field = value;
+                _footerBar.OnCommonButtonsChanged();
+            }
+        }
+    }
+    #endregion
+
+    #region public override
+    /// <summary>
+    /// Not implemented
+    /// </summary>
+    /// <returns>String.Empty</returns>
+    public sealed override string ToString()
+    {
+        return string.Empty;
+    }
+    #endregion
+}
---
+++ Source/Krypton Components/Krypton.Toolkit/Controls Toolkit/KryptonTaskDialog/Elements/KryptonTaskDialogElementFooterBarFooterProperties.cs
@@ -0,0 +1,153 @@
+#region BSD License
+/*
+ *
+ * New BSD 3-Clause License (https://github.com/Krypton-Suite/Standard-Toolkit/blob/master/LICENSE)
+ * Modifications by Peter Wagner (aka Wagnerp), Simon Coghlan (aka Smurf-IV), Giduac, Ahmed Abdelhameed, tobitege et al. 2025 - 2025. All rights reserved.
+ *
+ */
+#endregion
+
+namespace Krypton.Toolkit;
+
+/// <summary>
+/// Used internally by KryptonTaskDialogElementFooterBar
+/// </summary>
+public class KryptonTaskDialogElementFooterBarFooterProperties :
+    IKryptonTaskDialogElementFooterBar,
+    IKryptonTaskDialogElementIconType
+{
+    #region Fields
+    private KryptonTaskDialogElementFooterBar _footerBar;
+    private KryptonWrapLabel _footNoteText;
+    private string _expanderExpandedText;
+    private string _expanderCollapsedText;
+    private bool _enableExpanderControls;
+    private Action<bool> _onSizeChanged;
+    private Action _updateExpanderText;
+    private Action _updateExpanderEnabledState;
+    private Action _updateFootNoteIcon;
+    #endregion
+
+    #region Identity
+    /// <summary>
+    /// Default constructor.
+    /// </summary>
+    /// <param name="footerBar">FooterBar instance</param>
+    /// <param name="footNoteText">KryptonWarpLabel instance.</param>
+    /// <param name="onSizeChanged">FooterBar OnsizeChanged callback.</param>
+    /// <param name="updateExpanderText">FooterBar updateExpanderText callback.</param>
+    /// <param name="updateExpanderEnabledState">FooterBar updateExpanderEnabledState callback.</param>
+    /// <param name="updateFootNoteIcon">FooterBar updateFootNoteIcon callback.</param>
+    public KryptonTaskDialogElementFooterBarFooterProperties(KryptonTaskDialogElementFooterBar footerBar, KryptonWrapLabel footNoteText, 
+        Action<bool> onSizeChanged, Action updateExpanderText, Action updateExpanderEnabledState, Action updateFootNoteIcon
+        )
+    {
+        _footerBar                  = footerBar;
+        _footNoteText               = footNoteText;
+        _onSizeChanged              = onSizeChanged;
+        _updateExpanderText         = updateExpanderText;
+        _updateExpanderEnabledState = updateExpanderEnabledState;
+        _updateFootNoteIcon         = updateFootNoteIcon;
+        _expanderCollapsedText      = string.Empty;
+        _expanderExpandedText       = string.Empty;
+        _enableExpanderControls     = false;
+    }
+    #endregion
+
+    #region Public
+    /// <inheritdoc/>
+    public string FootNoteText
+    {
+        get => _footNoteText.Text;
+
+        set
+        {
+            if (_footNoteText.Text != value)
+            {
+                _footNoteText.Text = value;
+                _footNoteText.Invalidate();
+                _footerBar.LayoutDirty = true;
+                _onSizeChanged(false);
+            }
+        }
+    }
+
+    /// <inheritdoc/>
+    public string ExpanderExpandedText
+    {
+        get => _expanderExpandedText;
+
+        set
+        {
+            if (_expanderExpandedText != value)
+            {
+                _expanderExpandedText = value;
+                _footerBar.LayoutDirty = true;
+                _updateExpanderText();
+            }
+        }
+    }
+
+    /// <inheritdoc/>
+    public string ExpanderCollapsedText
+    {
+        get => _expanderCollapsedText;
+
+        set
+        {
+            if (_expanderCollapsedText != value)
+            {
+                _expanderCollapsedText = value;
+                _footerBar.LayoutDirty = true;
+                _updateExpanderText();
+            }
+        }
+    }
+
+    /// <summary>
+    /// Icon used to decorate the footnote.
+    /// </summary>
+    public KryptonTaskDialogIconType IconType
+    {
+        get => field;
+
+        set
+        {
+            if (field != value)
+            {
+                field = value;
+                _footerBar.LayoutDirty = true;
+                _updateFootNoteIcon();
+            }
+        }
+    }
+
+    /// <inheritdoc/>
+    public bool EnableExpanderControls
+    {
+        get => _enableExpanderControls;
+
+        set
+        {
+            if (_enableExpanderControls != value)
+            {
+                _enableExpanderControls = value;
+                _footerBar.LayoutDirty = true;
+                _updateExpanderEnabledState();
+                _onSizeChanged(false);
+            }
+        }
+    }
+    #endregion
+
+    #region public override
+    /// <summary>
+    /// Not implemented
+    /// </summary>
+    /// <returns>String.Empty</returns>
+    public sealed override string ToString()
+    {
+        return string.Empty;
+    }
+    #endregion
+}
