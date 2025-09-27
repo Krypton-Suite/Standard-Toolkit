092bf28821e7fef938d40d7b3148317428e158d5
Giduac <tweakdummy@protonmail.com>
2025-09-27 11:03:02 +0200

    1376-V100-Extending-KryptonTaskDialog - Updates Doxygen documentation - No buildlog since it stands apart form the toolkit.

---
+++ Source/Krypton Components/Krypton.Toolkit/Controls Toolkit/KryptonTaskDialog/Doxygen/html/class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_footer_bar_common_button_properties.js
@@ -0,0 +1,8 @@
+var class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_footer_bar_common_button_properties =
+[
+    [ "KryptonTaskDialogElementFooterBarCommonButtonProperties", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_footer_bar_common_button_properties.html#a4b90340513ebb80bf0df5d292e42eb47", null ],
+    [ "ToString", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_footer_bar_common_button_properties.html#aa562e2464c666811cd5d08c2102b453b", null ],
+    [ "AcceptButton", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_footer_bar_common_button_properties.html#ae36bd1fc7628e70f365f8e68ff4352b3", null ],
+    [ "Buttons", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_footer_bar_common_button_properties.html#aa8505a9c65463c1780556d7de7c2e985", null ],
+    [ "CancelButton", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_footer_bar_common_button_properties.html#adfebfb2af5eb5c60b94ec230118891f8", null ]
+];
---
+++ Source/Krypton Components/Krypton.Toolkit/Controls Toolkit/KryptonTaskDialog/Doxygen/html/class_krypton_1_1_toolkit_1_1_krypton_task_dialog_krypton_rich_text_box.js
@@ -0,0 +1,4 @@
+var class_krypton_1_1_toolkit_1_1_krypton_task_dialog_krypton_rich_text_box =
+[
+    [ "ProcessCmdKey", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_krypton_rich_text_box.html#a8f399fe66e69799febf98f5cfb190dea", null ]
+];
