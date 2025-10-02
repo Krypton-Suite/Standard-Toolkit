13678ee5c216db0d6c7ed54a89e37ca8c774b32a
Giduac <tweakdummy@protonmail.com>
2025-10-02 18:47:08 +0200

    V100-testform-startscreen-refresh Replacing the standard buttons approach with searchable commanlink buttons.

--- Source/Krypton Components/TestForm/Classes/RegistryAccess.cs
+++ Source/Krypton Components/TestForm/Classes/RegistryAccess.cs
@@ -1,39 +1,38 @@
-﻿namespace TestForm
+﻿namespace TestForm;
+
+using Microsoft.Win32;
+/// <summary>
+/// Simple class to handle program auto start when the user logs on.
+/// </summary>
+public class RegistryAccess
 {
-    using Microsoft.Win32;
+    // Registry access
+    private RegistryKey _registryKey;
+    private readonly string _registryPath;
+    private const string _rvLastFilterString = "LastFilterString";
+    private const string _rvDockTopRight = "DockTopRight";
+
     /// <summary>
-    /// Simple class to handle program auto start when the user logs on.
+    /// Default constructor
     /// </summary>
-    public class RegistryAccess
+    /// <exception cref="Exception">Throws an Exception when the registry key cannot be opened.</exception>
+    public RegistryAccess()
     {
-        // Registry access
-        private RegistryKey _registryKey;
-        private readonly string _registryPath;
-        private const string _rvLastFilterString = "LastFilterString";
-        private const string _rvDockTopRight = "DockTopRight";
+        _registryPath = @"Software\Krypton-Suite\Standard-ToolKit\TestForm";
+        _registryKey = Registry.CurrentUser.CreateSubKey(_registryPath)
+            ?? throw new Exception("Registry.CurrentUser.CreateSubKey() returned null.");
+    }
 
-        /// <summary>
-        /// Default constructor
-        /// </summary>
-        /// <exception cref="Exception">Throws an Exception when the registry key cannot be opened.</exception>
-        public RegistryAccess()
-        {
-            _registryPath = @"Software\Krypton-Suite\Standard-ToolKit\TestForm";
-            _registryKey = Registry.CurrentUser.CreateSubKey(_registryPath)
-                ?? throw new Exception("Registry.CurrentUser.CreateSubKey() returned null.");
-        }
+    public string LastFilterString
+    {
+        get => _registryKey.GetValue(_rvLastFilterString) as string ?? string.Empty;
+        set => _registryKey.SetValue(_rvLastFilterString, value);
+    }
 
-        public string LastFilterString
-        {
-            get => _registryKey.GetValue(_rvLastFilterString) as string ?? string.Empty;
-            set => _registryKey.SetValue(_rvLastFilterString, value);
-        }
-
-        public bool DockTopRight
-        {
-            get => (_registryKey.GetValue(_rvDockTopRight, "0") as string) == "1" ? true : false;
-            set => _registryKey.SetValue(_rvDockTopRight, value ? "1"  : "0");
-        }
+    public bool DockTopRight
+    {
+        get => (_registryKey.GetValue(_rvDockTopRight, "0") as string) == "1" ? true : false;
+        set => _registryKey.SetValue(_rvDockTopRight, value ? "1" : "0");
     }
 }
 
