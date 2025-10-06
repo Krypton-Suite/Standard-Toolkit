e8735c9eafbb998b093873a4eeed360830a8daf5
Giduac <tweakdummy@protonmail.com>
2025-10-06 21:52:32 +0200

    1376-V100-Extending-KryptonTaskDialog - Added a demo page with some samples of what you can with KryptonTaskDialog. - Added Bogus.Net faker lib to generate sequences of fake data.

---
+++ Source/Krypton Components/TestForm/KryptonTaskDialogDemo/DemoDialogs/KryptonTaskDialogDemoForm.Content1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestForm
+{
+    public partial class KryptonTaskDialogDemoForm
+    {
+        public void KryptonTaskDialogDemoFormContent()
+        {
+            KryptonTaskDialog taskDialog = new KryptonTaskDialog();
+
+            taskDialog.Dialog.Form.Text = "A Content sample with an image displayed";
+
+            string text = BogusData.BogusGenerator.GenerateLoremIpsumLines(5) + Environment.NewLine + Environment.NewLine +
+                BogusData.BogusGenerator.GenerateLoremIpsumLines(5) + Environment.NewLine + Environment.NewLine +
+                BogusData.BogusGenerator.GenerateLoremIpsumLines(5) + Environment.NewLine + Environment.NewLine +
+                BogusData.BogusGenerator.GenerateLoremIpsumLines(5);
+
+            taskDialog.Content.ContentImage.Image = KryptonTaskDialogDemo.KryptonTaskDialogDemoResources.lorem_ipsum;
+            taskDialog.Content.ContentImage.Size = new Size(150, 150);
+            taskDialog.Content.ContentImage.Visible = true;
+            taskDialog.Content.ContentImage.PositionedLeft = true;
+            taskDialog.Content.Visible = true;
+
+            taskDialog.Content.Text = text;
+
+            propGridKtd.SelectedObject = taskDialog;
+            taskDialog.Show(this);
+        }
+    }
+}
---
+++ Source/Krypton Components/TestForm/KryptonTaskDialogDemo/DemoDialogs/KryptonTaskDialogDemoFormFreeWheeler2.cs
@@ -0,0 +1,33 @@
+﻿namespace TestForm
+{
+    public partial class KryptonTaskDialogDemoForm
+    {
+        public DialogResult FreeWheeler2CheckedSetButtons()
+        {
+            KryptonTaskDialog taskDialog = new(500);
+            KryptonCheckSet checkSet = new();
+
+            taskDialog.Heading.Text = "Check That!";
+            taskDialog.Heading.IconType = KryptonTaskDialogIconType.CheckGreen;
+            taskDialog.Heading.Visible = true;
+
+            for( int i = 0; i < 10; i++)
+            {
+                var button = new KryptonCheckButton()
+                {
+                    Checked = false,
+                    Text = $"Button {i}",
+                    Size = new Size(75, 24)
+                };
+
+                checkSet.CheckButtons.Add(button);
+                taskDialog.FreeWheeler1.FlowLayoutPanel.Controls.Add(button);
+            }
+
+            taskDialog.FreeWheeler1.FlowLayoutPanel.FlowDirection = FlowDirection.LeftToRight;
+            taskDialog.FreeWheeler1.Visible = true;
+
+            return taskDialog.ShowDialog(this);
+        }
+    }
+}
