19b1f9482b72b150ed3b7ab4f8b9ebda601a220f
Peter Wagner <peterwagner@live.co.uk>
2025-10-08 12:31:00 +0100

    * License

--- Source/Krypton Components/TestForm/PoweredByButtonExample.Designer.cs
+++ Source/Krypton Components/TestForm/PoweredByButtonExample.Designer.cs
@@ -1,4 +1,13 @@
-﻿namespace TestForm
+﻿#region BSD License
+/*
+ *
+ *  New BSD 3-Clause License (https://github.com/Krypton-Suite/Standard-Toolkit/blob/master/LICENSE)
+ *  Modifications by Peter Wagner(aka Wagnerp) & Simon Coghlan(aka Smurf-IV), et al. 2025 - 2025. All rights reserved.
+ *
+ */
+#endregion
+
+namespace TestForm
 {
     partial class PoweredByButtonExample
     {
