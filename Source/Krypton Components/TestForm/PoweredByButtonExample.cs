89565449e722a12624218b5de950bd669e39f0b7
Peter Wagner <peterwagner@live.co.uk>
2025-10-08 12:20:25 +0100

    * Finalise #1009

---
+++ Source/Krypton Components/TestForm/PoweredByButtonExample.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestForm;
+
+public partial class PoweredByButtonExample : KryptonForm
+{
+    public PoweredByButtonExample()
+    {
+        InitializeComponent();
+
+        foreach (var value in Enum.GetValues(typeof(ToolkitSupportType)))
+        {
+            kryptonComboBox1.Items.Add(value);
+        }
+
+        kryptonComboBox1.SelectedIndex = 0;
+    }
+
+    private void PoweredByButtonExample_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    private void kchkShowReadmeButton_CheckedChanged(object sender, EventArgs e)
+    {
+        kryptonPoweredByButton1.ButtonValues.ShowReadmeButton = kchkShowReadmeButton.Checked;
+    }
+
+    private void kchkShowChangelogButton_CheckedChanged(object sender, EventArgs e)
+    {
+        kryptonPoweredByButton1.ButtonValues.ShowChangeLogButton = kchkShowChangelogButton.Checked;
+    }
+
+    private void kryptonComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        ToolkitSupportType support = (ToolkitSupportType)kryptonComboBox1.SelectedItem;
+
+        kryptonPoweredByButton1.ButtonValues.ToolkitSupportType = support;
+    }
+}
