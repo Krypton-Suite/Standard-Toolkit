cbada3cd9c0352a59787cd1ebe63d76c121ef800
giduac <96108132+giduac@users.noreply.github.com>
2025-10-19 11:40:32 +0200

    1376-V100-Extending-KryptonTaskDialog (#2589)
    - Remove Doxygen documentation
    - No changelog since it it not part of the toolkit

--- Source/Krypton Components/Krypton.Toolkit/Controls Toolkit/KryptonTaskDialog/Doxygen/html/_krypton_task_dialog_element_content_test_8cs.js
+++
@@ -1,4 +0,0 @@
-var _krypton_task_dialog_element_content_test_8cs =
-[
-    [ "Krypton.Toolkit.KryptonTaskDialogElementContentTest", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_content_test.html", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_content_test" ]
-];
--- Source/Krypton Components/Krypton.Toolkit/Controls Toolkit/KryptonTaskDialog/Doxygen/html/files_dup.js
+++
@@ -1,4 +0,0 @@
-var files_dup =
-[
-    [ "D:", "dir_275089585c7fc1b5fd5d7d42c69cb1da.html", "dir_275089585c7fc1b5fd5d7d42c69cb1da" ]
-];
