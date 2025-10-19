f899c365a7784d51452a521ef002170451292212
Giduac <tweakdummy@protonmail.com>
2025-10-19 11:38:02 +0200

    1376-V100-Extending-KryptonTaskDialog - Remove Doxygen documentation - No changelog since it it not part of the toolkit

--- Source/Krypton Components/Krypton.Toolkit/Controls Toolkit/KryptonTaskDialog/Doxygen/html/_krypton_task_dialog_element_progres_bar_8_properties_8cs.js
+++
@@ -1,4 +0,0 @@
-var _krypton_task_dialog_element_progres_bar_8_properties_8cs =
-[
-    [ "Krypton.Toolkit.KryptonTaskDialogElementProgresBarProperties", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_progres_bar_properties.html", "class_krypton_1_1_toolkit_1_1_krypton_task_dialog_element_progres_bar_properties" ]
-];
