41c96ed1d332fc90cc39cf6ff3b89b8cb35d22b5
Peter Wagner <peterwagner@live.co.uk>
2026-01-24 12:14:16 +0000

    * Disable styled scrollbars by default
    * Disable styled scrollbars by default

--- Source/Krypton Components/Krypton.Toolkit/Controls Toolkit/KryptonManager.cs
+++ Source/Krypton Components/Krypton.Toolkit/Controls Toolkit/KryptonManager.cs
@@ -407,14 +407,14 @@ public sealed class KryptonManager : Component
     /// </summary>
     [Category(@"Visuals")]
     [Description(@"Should scrollable controls use Krypton-themed scrollbars instead of native scrollbars.")]
-    [DefaultValue(true)]
+    [DefaultValue(false)]
     public bool GlobalUseKryptonScrollbars
     {
         get => UseKryptonScrollbars;
         set => UseKryptonScrollbars = value;
     }
     private bool ShouldSerializeGlobalUseKryptonScrollbars() => GlobalUseKryptonScrollbars;
-    private void ResetGlobalUseKryptonScrollbars() => GlobalUseKryptonScrollbars = true;
+    private void ResetGlobalUseKryptonScrollbars() => GlobalUseKryptonScrollbars = false;
 
 
     /// <summary>
