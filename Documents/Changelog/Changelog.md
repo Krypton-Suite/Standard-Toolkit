4214ed1b392d362ccf4c13fae1251a715c143107
giduac <96108132+giduac@users.noreply.github.com>
2026-03-13 05:10:44 +0100

    Revert "3201-V105-Fix-potential-memory-leaks-in-KryptonDataGridViewRatingColumn" (#3214)
    Revert "3201-V105-Fix-potential-memory-leaks-in-KryptonDataGridViewRatingColu…"
    
    This reverts commit 2ef34770b86b6789f72cc226a3739b8171d52f61.

--- Documents/Changelog/Changelog.md
+++ Documents/Changelog/Changelog.md
@@ -3,7 +3,7 @@
 ====
 
 ## 2026-04-20 - Build 2604 (Version 105-LTS - Patch 2) - April 2026
-* Resolved [#3201](https://github.com/Krypton-Suite/Standard-Toolkit/issues/3201), Resolves potential memory leaks in `KryptonDataGridViewRatingColumn`
+
 * Resolved [#3163](https://github.com/Krypton-Suite/Standard-Toolkit/issues/3163), Form Icon Misplaced in Contextual Tabs
 * Implemented [#3113](https://github.com/Krypton-Suite/Standard-Toolkit/issues/3113), TextBox item for KryptonContextMenu-Items
 * Resolved [#3124](https://github.com/Krypton-Suite/Standard-Toolkit/issues/3124), Docking: Loading configuration while form is hidden remover splitter
--- Source/Krypton Components/Krypton.Toolkit/Controls Toolkit/KryptonDataGridViewRatingCell.cs
+++ Source/Krypton Components/Krypton.Toolkit/Controls Toolkit/KryptonDataGridViewRatingCell.cs
@@ -5,7 +5,7 @@
  *  © Component Factory Pty Ltd, 2006 - 2016, (Version 4.5.0.0) All rights reserved.
  * 
  *  New BSD 3-Clause License (https://github.com/Krypton-Suite/Standard-Toolkit/blob/master/LICENSE)
- *  Modifications by Peter Wagner (aka Wagnerp), Simon Coghlan (aka Smurf-IV), Giduac, tobitege & Ahmed Abdelhameed et al. 2025 - 2026. All rights reserved.
+ *  Modifications by Peter Wagner (aka Wagnerp), Simon Coghlan (aka Smurf-IV), Giduac, tobitege & Ahmed Abdelhameed et al. 2025 - 2025. All rights reserved.
  *  
  */
 #endregion
@@ -111,12 +111,12 @@ public class KryptonDataGridViewRatingCell : KryptonDataGridViewTextBoxCell
             x = cellBounds.X;
             // Add three to cater for cell borders
             y = cellBounds.Top + 3;
-            // LTR aligns left, the rectangle for the image starts there
+            // LTR alignes left, the rectangle for the image starts there
             rectX = 0;
         }
         else
         {
-            // Determines on RTL whether "cellBounds.X + cellBounds.Width - image.Width"
+            // Determines on RTL wether "cellBounds.X + cellBounds.Width - image.Width"
             // amounts to a value which is farther to the left than cellBounds.X.
             // The max is the value that will serve as x when positioning the image on the cell.
             x = Math.Max(cellBounds.X, cellBounds.X + cellBounds.Width - image.Width);
