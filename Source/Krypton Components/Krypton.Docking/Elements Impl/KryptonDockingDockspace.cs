d1a09183f39625c4cbcd6ddd113de8827abc1a0d
tobitege <10787084+tobitege@users.noreply.github.com>
2026-05-28 08:55:20 +0200

    3227-V105-Docking fix followup (#3542)
    * Fix disposed docking space load handling (#3227)

--- Source/Krypton Components/Krypton.Docking/Elements Impl/KryptonDockingDockspace.cs
+++ Source/Krypton Components/Krypton.Docking/Elements Impl/KryptonDockingDockspace.cs
@@ -1,12 +1,12 @@
 ﻿#region BSD License
 /*
- * 
+ *
  * Original BSD 3-Clause License (https://github.com/ComponentFactory/Krypton/blob/master/LICENSE)
  *  © Component Factory Pty Ltd, 2006 - 2016, (Version 4.5.0.0) All rights reserved.
  *
  *  New BSD 3-Clause License (https://github.com/Krypton-Suite/Standard-Toolkit/blob/master/LICENSE)
- *  Modifications by Peter Wagner (aka Wagnerp), Simon Coghlan (aka Smurf-IV), Giduac & Ahmed Abdelhameed et al. 2017 - 2025. All rights reserved.
- *  
+ *  Modifications by Peter Wagner (aka Wagnerp), Simon Coghlan (aka Smurf-IV), tobitege, Giduac & Ahmed Abdelhameed et al. 2017 - 2026. All rights reserved.
+ *
  */
 #endregion
 
@@ -408,8 +408,6 @@ public class KryptonDockingDockspace : KryptonDockingSpace
         if (DockspaceControl.PageCount == 0)
         {
             DockspaceControl.Dispose();
-            // TODO: Is this safe ?. It means that whatever uses this afterwards could be accessing null things !
-            SpaceControl = null!;
         }
     }
     #endregion
@@ -417,7 +415,12 @@ public class KryptonDockingDockspace : KryptonDockingSpace
     #region Implementation
     private void OnDockspaceCellVisibleCountChanged(object? sender, EventArgs e)
     {
-        if (DockspaceControl.CellVisibleCount == 0)
+        if (sender is not KryptonDockspace dockspace)
+        {
+            return;
+        }
+
+        if (dockspace.CellVisibleCount == 0)
         {
             if (_cacheCellVisibleCount > 0)
             {
@@ -437,10 +440,15 @@ public class KryptonDockingDockspace : KryptonDockingSpace
 
     private void OnDockspaceCellCountChanged(object? sender, EventArgs e)
     {
-        // When all the cells (and so pages) have been removed we kill ourself
-        if (DockspaceControl.CellCount == 0)
+        if (sender is not KryptonDockspace dockspace)
         {
-            DockspaceControl.Dispose();
+            return;
+        }
+
+        // When all the cells (and so pages) have been removed we kill ourself
+        if (dockspace.CellCount == 0)
+        {
+            dockspace.Dispose();
         }
     }
 
