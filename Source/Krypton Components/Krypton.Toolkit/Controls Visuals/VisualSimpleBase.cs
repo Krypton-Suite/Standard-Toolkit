7a5f8e94b7c4f7803f1c2edeaf1ea21aed874894
tobitege <10787084+tobitege@users.noreply.github.com>
2026-06-01 08:39:27 +0200

    3580-V105-TLS Fix docking auto-sizing (#3630)
    * Fix docked header autosizing
    
    Preserve the dock-managed axis when autosizing simple Krypton controls so top and bottom docked headers keep their assigned width, while left and right docked controls keep their assigned height.
    
    Add a Header Examples repro with docked top and bottom KryptonHeader controls.
    
    Guard professional renderer callbacks against null event data and toolstrips.
    
    * Add changelog

--- Source/Krypton Components/Krypton.Toolkit/Controls Visuals/VisualSimpleBase.cs
+++ Source/Krypton Components/Krypton.Toolkit/Controls Visuals/VisualSimpleBase.cs
@@ -1,12 +1,12 @@
 ﻿#region BSD License
 /*
- * 
+ *
  * Original BSD 3-Clause License (https://github.com/ComponentFactory/Krypton/blob/master/LICENSE)
  *  © Component Factory Pty Ltd, 2006 - 2016, (Version 4.5.0.0) All rights reserved.
- * 
+ *
  *  New BSD 3-Clause License (https://github.com/Krypton-Suite/Standard-Toolkit/blob/master/LICENSE)
- *  Modifications by Peter Wagner (aka Wagnerp), Simon Coghlan (aka Smurf-IV), Giduac & Ahmed Abdelhameed et al. 2017 - 2025. All rights reserved.
- *  
+ *  Modifications by Peter Wagner (aka Wagnerp), Simon Coghlan (aka Smurf-IV), tobitege, Giduac & Ahmed Abdelhameed et al. 2017 - 2026. All rights reserved.
+ *
  */
 #endregion
 
@@ -164,14 +164,44 @@ public abstract class VisualSimpleBase : VisualControlBase
             {
                 if (GetAutoSizeMode() == AutoSizeMode.GrowAndShrink)
                 {
-                    width = preferredSize.Width;
-                    height = preferredSize.Height;
+                    switch (Dock)
+                    {
+                        case DockStyle.Top:
+                        case DockStyle.Bottom:
+                            height = preferredSize.Height;
+                            break;
+                        case DockStyle.Left:
+                        case DockStyle.Right:
+                            width = preferredSize.Width;
+                            break;
+                        case DockStyle.Fill:
+                            break;
+                        default:
+                            width = preferredSize.Width;
+                            height = preferredSize.Height;
+                            break;
+                    }
                 }
                 else
                 {
-                    // GrowOnly semantics: allow growth to preferred size, never force shrink.
-                    width = Math.Max(Width, preferredSize.Width);
-                    height = Math.Max(Height, preferredSize.Height);
+                    switch (Dock)
+                    {
+                        case DockStyle.Top:
+                        case DockStyle.Bottom:
+                            height = Math.Max(Height, preferredSize.Height);
+                            break;
+                        case DockStyle.Left:
+                        case DockStyle.Right:
+                            width = Math.Max(Width, preferredSize.Width);
+                            break;
+                        case DockStyle.Fill:
+                            break;
+                        default:
+                            // GrowOnly semantics: allow growth to preferred size, never force shrink.
+                            width = Math.Max(Width, preferredSize.Width);
+                            height = Math.Max(Height, preferredSize.Height);
+                            break;
+                    }
                 }
 
                 specified |= BoundsSpecified.Size;
--- Source/Krypton Components/TestForm/HeaderExamples.Designer.cs
+++ Source/Krypton Components/TestForm/HeaderExamples.Designer.cs
@@ -1,9 +1,9 @@
 ﻿#region BSD License
 /*
- * 
+ *
  *  New BSD 3-Clause License (https://github.com/Krypton-Suite/Standard-Toolkit/blob/master/LICENSE)
- *  Modifications by Peter Wagner(aka Wagnerp) & Simon Coghlan(aka Smurf-IV), et al. 2024 - 2025. All rights reserved. 
- *  
+ *  Modifications by Peter Wagner(aka Wagnerp) & Simon Coghlan(aka Smurf-IV), tobitege et al. 2024 - 2026. All rights reserved.
+ *
  */
 #endregion
 
@@ -37,31 +37,58 @@ namespace TestForm
         /// </summary>
         private void InitializeComponent()
         {
-            this.kryptonHeader1 = new Krypton.Toolkit.KryptonHeader();
+            this.kryptonHeaderTop = new Krypton.Toolkit.KryptonHeader();
+            this.kryptonHeaderBottom = new Krypton.Toolkit.KryptonHeader();
+            this.labelInstructions = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // kryptonHeader1
-            // 
-            this.kryptonHeader1.Location = new System.Drawing.Point(13, 13);
-            this.kryptonHeader1.Name = "kryptonHeader1";
-            this.kryptonHeader1.Size = new System.Drawing.Size(251, 31);
-            this.kryptonHeader1.TabIndex = 2;
-            this.kryptonHeader1.Values.Heading = "kryptonHeader1";
-            // 
+            //
+            // kryptonHeaderTop
+            //
+            this.kryptonHeaderTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.kryptonHeaderTop.Location = new System.Drawing.Point(0, 0);
+            this.kryptonHeaderTop.Name = "kryptonHeaderTop";
+            this.kryptonHeaderTop.Size = new System.Drawing.Size(800, 31);
+            this.kryptonHeaderTop.TabIndex = 0;
+            this.kryptonHeaderTop.Values.Description = "AutoSize = true, Dock = Top";
+            this.kryptonHeaderTop.Values.Heading = "Top docked header";
+            //
+            // kryptonHeaderBottom
+            //
+            this.kryptonHeaderBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.kryptonHeaderBottom.Location = new System.Drawing.Point(0, 419);
+            this.kryptonHeaderBottom.Name = "kryptonHeaderBottom";
+            this.kryptonHeaderBottom.Size = new System.Drawing.Size(800, 31);
+            this.kryptonHeaderBottom.TabIndex = 1;
+            this.kryptonHeaderBottom.Values.Description = "AutoSize = true, Dock = Bottom";
+            this.kryptonHeaderBottom.Values.Heading = "Bottom docked header";
+            //
+            // labelInstructions
+            //
+            this.labelInstructions.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelInstructions.Location = new System.Drawing.Point(0, 31);
+            this.labelInstructions.Name = "labelInstructions";
+            this.labelInstructions.Size = new System.Drawing.Size(800, 388);
+            this.labelInstructions.TabIndex = 2;
+            this.labelInstructions.Text = "Resize this form. Both KryptonHeader controls should keep the full client width while AutoSize keeps their preferred height.";
+            this.labelInstructions.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            //
             // HeaderExamples
-            // 
+            //
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.kryptonHeader1);
+            this.Controls.Add(this.labelInstructions);
+            this.Controls.Add(this.kryptonHeaderBottom);
+            this.Controls.Add(this.kryptonHeaderTop);
             this.Name = "HeaderExamples";
             this.Text = "HeaderExamples";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-        private KryptonHeader kryptonHeader1;
+        private KryptonHeader kryptonHeaderTop;
+        private KryptonHeader kryptonHeaderBottom;
+        private System.Windows.Forms.Label labelInstructions;
     }
 }
