5728bd8f397adc86c02a86e9e11054480c88ff86
Peter Wagner <peterwagner@live.co.uk>
2026-08-23 12:56:30 +0100

    * Add BSD headers to interop helpers
    Added the repository BSD license header to the General interop helper files in Krypton.Interop. This keeps the source files consistent with the project’s licensing and header conventions without changing behavior or APIs.

--- Source/Krypton Components/Krypton.Interop/General/Libraries.cs
+++ Source/Krypton Components/Krypton.Interop/General/Libraries.cs
@@ -1,4 +1,13 @@
-﻿namespace Krypton.Toolkit;
+﻿#region BSD License
+/*
+ *
+ *  New BSD 3-Clause License (https://github.com/Krypton-Suite/Standard-Toolkit/blob/master/LICENSE)
+ *  Modifications by Peter Wagner (aka Wagnerp), Simon Coghlan (aka Smurf-IV), Giduac, Ahmed Abdelhameed, tobitege, KamaniAR, Lesandro Gotardo (aka lesandrog), Jorge A. Avilés (aka mcpbcs) et al. 2026 - 2026. All rights reserved.
+ *
+ */
+#endregion
+
+namespace Krypton.Toolkit;
 
 /// <summary>
 /// Contains string constants for Windows API library names used in Platform Invoke declarations.
