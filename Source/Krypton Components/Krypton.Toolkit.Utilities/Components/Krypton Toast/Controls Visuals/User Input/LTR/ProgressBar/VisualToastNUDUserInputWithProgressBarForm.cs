0fef29270297782e85208e26127b7c6b794a0703
Peter Wagner <peterwagner@live.co.uk>
2026-09-07 18:42:30 +0100

    * Tweaks

--- Source/Krypton Components/Krypton.Toolkit.Utilities/Components/Krypton Toast/Controls Visuals/User Input/LTR/ProgressBar/VisualToastNUDUserInputWithProgressBarForm.cs
+++ Source/Krypton Components/Krypton.Toolkit.Utilities/Components/Krypton Toast/Controls Visuals/User Input/LTR/ProgressBar/VisualToastNUDUserInputWithProgressBarForm.cs
@@ -50,18 +50,18 @@ internal partial class VisualToastNUDUserInputWithProgressBarForm : VisualToastB
 
     private void UpdateBorderColors()
     {
-        StateCommon!.Border.Color1 = _data.BorderColor1 ?? GlobalStaticValues.EMPTY_COLOR;
+        StateCommon!.Border.Color1 = _data.BorderColor1 ?? SharedStaticVariables.EMPTY_COLOR;
 
-        StateCommon!.Border.Color2 = _data.BorderColor2 ?? GlobalStaticValues.EMPTY_COLOR;
+        StateCommon!.Border.Color2 = _data.BorderColor2 ?? SharedStaticVariables.EMPTY_COLOR;
     }
 
     private void UpdateText()
     {
-        GlobalStaticValues.ApplyToastRichTextContentColor(krtbNotificationContentText);
+        CommonFeatures.ApplyToastRichTextContentColor(krtbNotificationContentText);
 
-        klblHeader.Text = _data.NotificationTitle ?? GlobalStaticValues.DEFAULT_EMPTY_STRING;
+        klblHeader.Text = _data.NotificationTitle ?? SharedStaticVariables.DEFAULT_EMPTY_STRING;
 
-        krtbNotificationContentText.Text = _data.NotificationContent ?? GlobalStaticValues.DEFAULT_EMPTY_STRING;
+        krtbNotificationContentText.Text = _data.NotificationContent ?? SharedStaticVariables.DEFAULT_EMPTY_STRING;
     }
 
     private void UpdateInitialValues()
